9e9fd14281dee58de00189fb4fbb9d1ed16e39cc
Yuriy Volodin <vol1ura@gmail.com>
2022-07-05 17:10:59 +0300

    change layout of admin form for result

--- app/admin/results.rb
+++ app/admin/results.rb
@@ -23,13 +23,13 @@ ActiveAdmin.register Result do
   form partial: 'form'
 
   before_update do |result|
-    if params[:parkrun_code].present?
-      athlete = Athlete.find_by parkrun_code: params[:parkrun_code].to_s.strip
+    if params[:parkrun_code].present? || params[:fiveverst_code].present?
+      athlete = Athlete.find_by parkrun_code: params[:parkrun_code].to_s.strip if params[:parkrun_code].present?
+      athlete ||= Athlete.find_by fiveverst_code: params[:fiveverst_code].to_s.strip if params[:fiveverst_code].present?
       if athlete
         result.athlete = athlete
       else
-        flash[:alert] = "Участник с parkrun id='#{params[:parkrun_code]}' не найден. \
-        # Проверьте номер или сначала создайте такого участника."
+        flash[:alert] = 'Участник с указанными ID не найден. Проверьте номера или сначала создайте такого участника.'
       end
     end
   end
