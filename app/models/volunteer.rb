1575a416d98b514f62afe1cca8d26455556d0f52
Yuriy Volodin <vol1ura@gmail.com>
2022-07-05 17:28:21 +0300

    add new roles for volunteers

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class Volunteer < ApplicationRecord
-  ROLES = { director: 0, marshal: 1, timer: 2, photograph: 3, tokens: 4 }.freeze
+  ROLES = {
+    director: 0, marshal: 1, timer: 2, photograph: 3, tokens: 4, scanner: 5,
+    instructor: 6, marking_maker: 7, event_closer: 8, marking_picker: 9, cards_sorter: 10,
+    bike_leader: 11, pacemaker: 12, results_handler: 13, equipment_supplier: 14, public_relations: 15
+  }.freeze
 
   belongs_to :activity
   belongs_to :athlete
