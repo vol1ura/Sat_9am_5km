e630a138810c085e38fce82dd55218d36b0850b3
Yuriy Volodin <vol1ura@gmail.com>
2022-07-20 08:55:06 +0300

    add tests for badges and trophies

--- spec/models/badge_spec.rb
+++ spec/models/badge_spec.rb
@@ -1,3 +1,11 @@
 RSpec.describe Badge, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.not_to be_valid }
+
+  it 'valid with name and picture link' do
+    badge = described_class.new(
+      name: Faker::Books::Dune.title,
+      picture_link: Faker::File.dir(segment_count: 3, root: nil, directory_separator: '/')
+    )
+    expect(badge).to be_valid
+  end
 end
--- spec/models/trophy_spec.rb
+++ spec/models/trophy_spec.rb
@@ -1,3 +1,19 @@
 RSpec.describe Trophy, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:badge) { create :badge }
+  let(:some_athlete) { create :athlete }
+
+  it { is_expected.not_to be_valid }
+
+  it 'valid with badge and uniq athlete' do
+    other_athlete = create :athlete
+    create :trophy, badge: badge, athlete: other_athlete
+    trophy = build :trophy, badge: badge, athlete: some_athlete
+    expect(trophy).to be_valid
+  end
+
+  it 'invalid with badge and same athlete' do
+    create :trophy, badge: badge, athlete: some_athlete
+    trophy = build :trophy, badge: badge, athlete: some_athlete
+    expect(trophy).not_to be_valid
+  end
 end
