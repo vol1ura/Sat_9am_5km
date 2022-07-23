b968839f96418e585f806ab4f75d1493b9af9fe0
Yuriy Volodin <vol1ura@gmail.com>
2022-07-23 17:11:37 +0300

    add pace column to protocol

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,13 @@ module ApplicationHelper
     time.strftime(time.hour.zero? ? '%M:%S' : '%H:%M:%S')
   end
 
+  def human_result_pace(time)
+    return unless time
+
+    avg_sec = ((((time.hour * 60) + time.min) * 60) + time.sec) / 5
+    format '%d:%02d', avg_sec / 60, avg_sec % 60
+  end
+
   def human_activity_name(activity)
     "#{activity.date} - #{activity.event.name}"
   end
