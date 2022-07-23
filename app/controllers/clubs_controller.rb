7bc4cf06223b3063c8d8d678676e5a386828b14a
Yuriy Volodin <vol1ura@gmail.com>
2022-07-23 15:13:10 +0300

    bump bootstrap to 5.2.0

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -8,6 +8,6 @@ class ClubsController < ApplicationController
   def show
     @club = Club.find(params[:id])
     @count_results = Athlete.left_joins(:results).where(club: @club).group('athletes.id').count('results.id')
-    @athletes = Athlete.where(club: @club)
+    @athletes = Athlete.where(club: @club).order(:name)
   end
 end
