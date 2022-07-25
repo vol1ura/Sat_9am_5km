3f5b0ef982af69733757582bf28067d03a76554a
Yuriy Volodin <vol1ura@gmail.com>
2022-07-25 23:43:26 +0300

    modify main menu and static content

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe '/pages', type: :request do
   describe 'GET /pages' do
     it 'renders a successful response' do
-      get page_url(page: %w[index about support join rules].sample)
+      get page_url(page: %w[index about support rules].sample)
       expect(response).to be_successful
     end
   end
