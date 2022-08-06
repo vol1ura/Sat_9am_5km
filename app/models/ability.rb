e6720364927c23ee29906f751d1ac6692902232e
Yuriy Volodin <vol1ura@gmail.com>
2022-08-07 01:16:39 +0300

    refactor Ability class

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,19 +7,24 @@ class Ability
     return unless user
     can(:manage, :all) and return if user.admin?
 
-    can :read, ActiveAdmin::Page, name: 'Dashboard', namespace_name: 'admin'
-    can :manage, User, id: user.id
+    @user = user
+    base_permissions
+    special_permissions
+  end
 
+  private
+
+  attr_reader :user
+
+  def base_permissions
+    can :read, ActiveAdmin::Page, name: 'Dashboard', namespace_name: 'admin'
+    can %i[read update], User, id: user.id
+  end
+
+  def special_permissions
     user.permissions.each do |permission|
-      params = {}
-      params[:id] = permission.subject_id if permission.subject_id
-      if permission.subject_class == 'Activity'
-        params[:event_id] = permission.event_id if permission.event_id
-        can permission.action.to_sym, Activity, params
-        cannot :destroy, Activity, published: true
-      else
-        can permission.action.to_sym, permission.subject_class.constantize, params
-      end
+      can permission.action.to_sym, permission.subject_class.constantize, permission.params
     end
+    cannot :destroy, Activity, published: true
   end
 end
