031483420d0183a2d222aa0f789deb23e85338d6
Yuriy Volodin <vol1ura@gmail.com>
2022-08-07 11:18:25 +0300

    add tests for results admin actions

--- spec/factories/results.rb
+++ spec/factories/results.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     sequence :position
     athlete
     activity
+    total_time { "0:18:#{10 + (2 * position)}" }
   end
 end
--- spec/requests/admin/results_spec.rb
+++ spec/requests/admin/results_spec.rb
@@ -22,4 +22,52 @@ RSpec.describe '/admin/results', type: :request do
       expect(response).to be_successful
     end
   end
+
+  context 'with manage permission' do
+    let(:activity) { create :activity }
+    let(:results) { create_list :result, 3, activity: activity }
+
+    before do
+      create :permission, user: user, action: 'manage', subject_class: 'Result'
+    end
+
+    describe 'DELETE /admin/activities/1/results/1/drop_time' do
+      it 'change result total_time to next result total_time' do
+        second_time = results.second.total_time
+        delete drop_time_admin_activity_result_url(activity, results.first, format: :js)
+        expect(results.first.reload.total_time.strftime('%H:%M:%S')).to eq second_time.strftime('%H:%M:%S')
+        expect(results.last.reload.total_time).to be_nil
+      end
+    end
+
+    describe 'DELETE /admin/activities/1/results/1/drop_athlete' do
+      it 'change result total_time to next result total_time' do
+        second_athlete = results.second.athlete
+        delete drop_athlete_admin_activity_result_url(activity, results.first, format: :js)
+        expect(results.first.reload.athlete).to eq second_athlete
+        expect(results.last.reload.athlete).to be_nil
+      end
+    end
+  end
+
+  context 'without manage permission' do
+    let(:activity) { create :activity }
+    let(:results) { create_list :result, 3, activity: activity }
+
+    describe 'DELETE /admin/activities/1/results/1/drop_time' do
+      it 'renders alert' do
+        delete drop_time_admin_activity_result_url(activity, results.first, format: :js)
+        expect(response).to be_successful
+        expect(response.body).to include 'Не удалось'
+      end
+    end
+
+    describe 'DELETE /admin/activities/1/results/1/drop_athlete' do
+      it 'renders alert' do
+        delete drop_athlete_admin_activity_result_url(activity, results.first, format: :js)
+        expect(response).to be_successful
+        expect(response.body).to include 'Не удалось'
+      end
+    end
+  end
 end
