dcf3d8a585bbbfd562569c94b6392bb71dbe2682
Yuriy Volodin <vol1ura@gmail.com>
2022-08-07 17:17:41 +0300

    fix layout according w3c

--- app/javascript/controllers/protocol_controller.js
+++ app/javascript/controllers/protocol_controller.js
@@ -4,7 +4,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["absolute", "male", "female"]
 
-  connect() {
+  initialize() {
     this.maleTarget.innerHTML = this.absoluteTarget.innerHTML
     this.filterTableByMale(this.maleTarget, 'true')
     this.femaleTarget.innerHTML = this.absoluteTarget.innerHTML
@@ -14,7 +14,7 @@ export default class extends Controller {
   filterTableByMale(table, is_male) {
     let new_position = 0
     table.querySelectorAll('tbody tr').forEach(row => {
-      if (row.getAttribute('male') !== is_male) {
+      if (row.dataset.male !== is_male) {
         row.parentNode.removeChild(row)
       } else {
         row.querySelector('.position').innerText = ++new_position
