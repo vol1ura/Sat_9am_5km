4a5bbb7f52e8d8e7646f0765c1d63e2d76dc6297
Yuriy Volodin <vol1ura@gmail.com>
2022-08-07 11:18:25 +0300

    add tests for results admin actions

--- spec/models/permission_spec.rb
+++ spec/models/permission_spec.rb
@@ -1,8 +1,24 @@
 RSpec.describe Permission, type: :model do
-  it { is_expected.not_to be_valid }
+  describe 'validations' do
+    it { is_expected.not_to be_valid }
 
-  it 'valid with user, action and subject_class' do
-    permission = create :permission
-    expect(permission).to be_valid
+    it 'valid with user, action and subject_class' do
+      permission = create :permission
+      expect(permission).to be_valid
+    end
+  end
+
+  describe '#params' do
+    let(:event) { create :event }
+
+    it 'returns params with id key' do
+      permission = create :permission, subject_id: 1
+      expect(permission.params).to include(id: 1)
+    end
+
+    it 'returns params with event_id key' do
+      permission = create :permission, event: event, subject_class: 'Activity'
+      expect(permission.params).to include(event_id: event.id)
+    end
   end
 end
