824fae20d3c110f09a36ce998b32d84178c987b5
Yuriy Volodin <vol1ura@gmail.com>
2022-08-11 00:01:01 +0300

    fix vcr tape and bump webmock gem

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -1,5 +1,9 @@
 RSpec.describe '/pages', type: :request do
   describe 'GET /pages' do
+    before do
+      srand(1000)
+    end
+
     %w[index about support rules].each do |page|
       it "renders page #{page} with successful response", vcr: page == 'about' do
         get page_url(page: page)
