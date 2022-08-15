eaeaabe0daea4e7747c55bd221113e0012c392f8
Yuriy Volodin <vol1ura@gmail.com>
2022-08-15 09:47:24 +0300

    extract includes from result model

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -2,7 +2,7 @@
 
 class ResultsController < ApplicationController
   def top
-    @male_results = Result.top(male: true, limit: 50)
-    @female_results = Result.top(male: false, limit: 50)
+    @male_results = Result.includes(activity: :event, athlete: :club).top(male: true, limit: 50)
+    @female_results = Result.includes(activity: :event, athlete: :club).top(male: false, limit: 50)
   end
 end
