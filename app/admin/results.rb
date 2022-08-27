ad66e0f28f6c69453fd1df53e4dc84240b7344de
Yuriy Volodin <vol1ura@gmail.com>
2022-08-27 23:22:29 +0300

    refactor result editor in admin panel

--- app/admin/results.rb
+++ app/admin/results.rb
@@ -3,7 +3,7 @@
 ActiveAdmin.register Result do
   belongs_to :activity
 
-  includes :athlete, activity: :event
+  includes :athlete, :activity
 
   actions :all
 
@@ -13,17 +13,36 @@ ActiveAdmin.register Result do
 
   permit_params :total_time, :position, :athlete_id
 
-  index download_links: false do
+  index download_links: false, title: -> { "Редактор протокола #{l(Activity.find(params[:activity_id]).date)}" } do
     selectable_column
     column :position
-    column :athlete
-    column(:total_time) { |r| human_result_time(r.total_time) }
+    column :athlete do |r|
+      if r.athlete
+        link_to r.athlete.name, admin_athlete_path(r.athlete), target: '_blank', rel: 'noopener'
+      else
+        link_to 'БЕЗ ТОКЕНА (создать)', new_admin_athlete_path(result_id: r.id), target: '_blank', rel: 'noopener'
+      end
+    end
+    column :total_time do |r|
+      link_to human_result_time(r.total_time),
+              edit_admin_activity_result_path(r.activity, r),
+              target: '_blank', rel: 'noopener'
+    end
     column('Изменение позиции') do |r|
       render partial: 'up_down', locals: { activity: r.activity, result: r }
     end
-    column('Забег') { |r| human_activity_name(r.activity) }
     actions do |r|
       render partial: 'drops', locals: { activity: r.activity, result: r }
+    end
+  end
+
+  sidebar I18n.t('active_admin.results.explanation.title'), only: :index do
+    ul do
+      li I18n.t('active_admin.results.explanation.unknown_athlete')
+      li I18n.t('active_admin.results.explanation.without_token')
+      li I18n.t('active_admin.results.explanation.delete_result')
+      li I18n.t('active_admin.results.explanation.delete_time')
+      li I18n.t('active_admin.results.explanation.delete_athlete')
     end
   end
 
@@ -32,9 +51,9 @@ ActiveAdmin.register Result do
   form partial: 'form'
 
   before_update do |result|
-    if params[:parkrun_code].present? || params[:fiveverst_code].present?
-      athlete = Athlete.find_by parkrun_code: params[:parkrun_code].to_s.strip if params[:parkrun_code].present?
-      athlete ||= Athlete.find_by fiveverst_code: params[:fiveverst_code].to_s.strip if params[:fiveverst_code].present?
+    if params[:code].present?
+      athlete_code = Athlete::PersonalCode.new(params[:code].to_i)
+      athlete = Athlete.find_by(**athlete_code.to_params)
       if athlete
         result.athlete = athlete
       else
@@ -46,13 +65,13 @@ ActiveAdmin.register Result do
   member_action :up, method: :put, if: proc { can? :update, Result } do
     @pred_result = resource.swap_with_position!(resource.position.pred)
   rescue StandardError
-    render js: "alert('#{I18n.t 'active_admin.results.cannot_move_result'}')"
+    render js: "alert('#{t 'active_admin.results.cannot_move_result'}')"
   end
 
   member_action :down, method: :put, if: proc { can? :update, Result } do
     @next_result = resource.swap_with_position!(resource.position.next)
   rescue StandardError
-    render js: "alert('#{I18n.t 'active_admin.results.cannot_move_result'}')"
+    render js: "alert('#{t 'active_admin.results.cannot_move_result'}')"
   end
 
   member_action :drop_time, method: :delete, if: proc { can? :manage, Result } do
