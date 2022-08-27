748e596dbbf1529b51f131dcb28aabb74d7a1304
Yuriy Volodin <vol1ura@gmail.com>
2022-08-27 14:59:10 +0300

    add note to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_20_125347) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_27_114406) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -135,6 +135,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_20_125347) do
     t.string "first_name"
     t.string "last_name"
     t.integer "role"
+    t.string "note"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
