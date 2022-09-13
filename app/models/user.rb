5be0aba5a289a5b0c54a9034c489850fbeee40a8
Yuriy Volodin <vol1ura@gmail.com>
2022-09-13 08:48:32 +0300

    configure registration forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :recoverable, :rememberable, :validatable, :confirmable, :lockable, :registerable
 
   has_one :athlete, dependent: :nullify
+  accepts_nested_attributes_for :athlete, reject_if: :all_blank
   has_many :permissions, dependent: :destroy
 
   validates :telegram_id, uniqueness: true, allow_nil: true
