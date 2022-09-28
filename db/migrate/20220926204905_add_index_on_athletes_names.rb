4ad4769065d742da000b8e0115034564fcc8b32a
Yuriy Volodin <vol1ura@gmail.com>
2022-09-28 11:43:05 +0300

    small fixes

--- db/migrate/20220926204905_add_index_on_athletes_names.rb
+++ db/migrate/20220926204905_add_index_on_athletes_names.rb
@@ -1,5 +1,11 @@
 class AddIndexOnAthletesNames < ActiveRecord::Migration[7.0]
-  def change
+  def up
+    enable_extension :pg_trgm
     add_index :athletes, :name, opclass: :gist_trgm_ops, using: :gist
+  end
+
+  def down
+    remove_index :athletes, :name
+    disable_extension :pg_trgm
   end
 end
