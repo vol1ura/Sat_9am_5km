29c995a929acf8854d37e3c28ede464d5733bc0b
Yuriy Volodin <vol1ura@gmail.com>
2022-10-11 10:50:50 +0300

    add api endoint to link user and athlete

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -5,7 +5,8 @@ RSpec.describe '/user', type: :request do
         user: {
           email: Faker::Internet.free_email,
           password: Faker::Internet.password(min_length: 6),
-          telegram_id: Faker::Number.number(digits: 8)
+          telegram_id: Faker::Number.number(digits: 8),
+          telegram_user: Faker::Internet.username
         }
       }
     end
@@ -61,6 +62,65 @@ RSpec.describe '/user', type: :request do
         expect do
           post user_path(format: :json), params: user_attributes.merge(athlete_id: athlete.id), headers: valid_headers
         end.to change(User, :count).by(1)
+      end
+    end
+  end
+
+  describe 'PUT /user' do
+    let(:user) { create :user }
+    let(:athlete_attributes) do
+      {
+        athlete: {
+          name: Faker::Name.name,
+          parkrun_code: Faker::Number.number(digits: 7)
+        }
+      }
+    end
+
+    context 'when header api key is invalid' do
+      it 'returns unauthorized response' do
+        put user_path(format: :json),
+            params: { user_id: user.id }.merge(athlete_attributes),
+            headers: {
+              'Accept' => 'application/json',
+              'Authorization' => Faker::Crypto.sha256
+            }
+        expect(response).to have_http_status :unauthorized
+      end
+    end
+
+    context 'with valid header api key' do
+      let(:valid_headers) do
+        {
+          'Accept' => 'application/json',
+          'Authorization' => Rails.application.credentials.internal_api_key
+        }
+      end
+      let(:user_attributes) do
+        {
+          user_id: user.id,
+          user: {
+            telegram_id: Faker::Number.number(digits: 8),
+            telegram_user: Faker::Internet.username
+          }
+        }
+      end
+
+      it 'renders successful response' do
+        put user_path(format: :json), params: user_attributes.merge(athlete_attributes), headers: valid_headers
+        expect(response).to be_successful
+      end
+
+      it 'links user with athlete' do
+        expect do
+          put user_path(format: :json), params: user_attributes.merge(athlete_attributes), headers: valid_headers
+        end.to change(Athlete, :count).by(1)
+      end
+
+      it 'creates user with linked athlete' do
+        athlete = create :athlete
+        put user_path(format: :json), params: user_attributes.merge(athlete_id: athlete.id), headers: valid_headers
+        expect(athlete.reload.user).to eq user
       end
     end
   end
