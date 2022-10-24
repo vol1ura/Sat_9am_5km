afbdcd63f75e4bcca731d63ea6886520eca54efb
Yuriy Volodin <vol1ura@gmail.com>
2022-10-24 08:50:02 +0300

    add tests with tourists badges

--- spec/jobs/athlete_awarding_job_spec.rb
+++ spec/jobs/athlete_awarding_job_spec.rb
@@ -3,27 +3,48 @@ RSpec.describe AthleteAwardingJob, type: :job do
 
   fixtures :badges
 
-  let(:event) { create :event }
   let(:athlete) { create :athlete }
-  let(:activity) { create :activity, date: Time.zone.today, event: event }
 
-  before do
-    24.times do |idx|
-      activity = create :activity, event: event, date: idx.next.week.ago
-      create :result, athlete: athlete, activity: activity
-      create :volunteer, athlete: athlete, activity: activity
+  context 'with participants badges' do
+    let(:event) { create :event }
+    let(:activity) { create :activity, date: Time.zone.today, event: event }
+
+    before do
+      24.times do |idx|
+        activity = create :activity, event: event, date: idx.next.week.ago
+        create :result, athlete: athlete, activity: activity
+        create :volunteer, athlete: athlete, activity: activity
+      end
+    end
+
+    it 'performs immediately' do
+      expect { described_class.perform_later(activity.id) }.to have_enqueued_job.on_queue('default').at(:no_wait)
+    end
+
+    it 'creates new trophies' do
+      create :result, activity: activity, athlete: athlete
+      create :volunteer, activity: activity, athlete: athlete
+      expect do
+        described_class.perform_now(activity.id)
+      end.to change(athlete.trophies, :count).by(2)
     end
   end
 
-  it 'performs immediately' do
-    expect { described_class.perform_later(activity.id) }.to have_enqueued_job.on_queue('default').at(:no_wait)
-  end
+  context 'with tourist badges' do
+    it 'creates runner badge' do
+      create_list :result, 5, athlete: athlete
+      last_activity_id = athlete.results.joins(:activity).order('activities.date').last.activity_id
+      expect do
+        described_class.perform_now(last_activity_id)
+      end.to change(athlete.trophies, :count).by(1)
+    end
 
-  it 'creates new trophies' do
-    create :result, activity: activity, athlete: athlete
-    create :volunteer, activity: activity, athlete: athlete
-    expect do
-      described_class.perform_now(activity.id)
-    end.to change(athlete.trophies, :count).by(2)
+    it 'creates volunteer badge' do
+      create_list :volunteer, 5, :with_published_activity, athlete: athlete
+      last_activity_id = athlete.volunteering.reorder('activity.date').last.activity_id
+      expect do
+        described_class.perform_now(last_activity_id)
+      end.to change(athlete.trophies, :count).by(1)
+    end
   end
 end
