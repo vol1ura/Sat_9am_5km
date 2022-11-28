0f499ef17bd32f13628a2323249d8d8e958d7b5b
Yuriy Volodin <vol1ura@gmail.com>
2022-11-28 14:50:44 +0300

    fix counter cache issue on activity resources

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -5,11 +5,20 @@ class ActivitiesController < ApplicationController
     today = Date.current
     date_since = today.sunday? ? today.monday : today.prev_week
     @activities = Activity.published.includes(:event).where(date: date_since..).order('activities.date DESC', 'events.name')
+    @results_count = Result.where(activity: @activities.unscope(:order, :includes)).group(:activity_id).count
   end
 
   def show
     @activity = Activity.find(params[:id])
-    @counts = Result.joins(athlete: :activities).where(athlete: { activities: @activity }).published.group(:athlete).count
+    @results_count = Result.joins(athlete: :activities)
+                           .where(athlete: { activities: @activity })
+                           .published
+                           .group(:athlete)
+                           .count
+    @volunteering_count = Volunteer.joins(:activity, athlete: :activities)
+                                   .where(activity: { published: true }, athlete: { activities: @activity })
+                                   .group(:athlete)
+                                   .count
     @results = @activity.results.includes(athlete: :club).order(:position)
   end
 end
