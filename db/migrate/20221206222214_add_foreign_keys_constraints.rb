350de41c48875e5edd97ee82c0c24f9d72691663
Yuriy Volodin <vol1ura@gmail.com>
2022-12-07 01:36:47 +0300

    add foreign keys constraints

---
+++ db/migrate/20221206222214_add_foreign_keys_constraints.rb
@@ -0,0 +1,10 @@
+class AddForeignKeysConstraints < ActiveRecord::Migration[7.0]
+  def change
+    remove_foreign_key :athletes, :clubs
+    remove_foreign_key :athletes, :users
+    remove_foreign_key :results, :athletes
+    add_foreign_key :athletes, :clubs, on_delete: :nullify
+    add_foreign_key :athletes, :users, on_delete: :nullify
+    add_foreign_key :results, :athletes, on_delete: :nullify
+  end
+end
