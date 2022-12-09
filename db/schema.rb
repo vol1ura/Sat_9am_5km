16803261b90b44e42b96a14f73b8ec4393acd6eb
Yuriy Volodin <vol1ura@gmail.com>
2022-12-09 13:58:58 +0300

    add kuskovo badge

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_06_222214) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_09_105200) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
