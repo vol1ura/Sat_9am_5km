d1b4411f7f8f58151c60d5735b712a9d76e89553
Yuriy Volodin <vol1ura@gmail.com>
2022-12-25 02:18:03 +0300

    redefine destroy action for athletes admin controller

--- spec/requests/admin/athletes_spec.rb
+++ spec/requests/admin/athletes_spec.rb
@@ -38,6 +38,7 @@ RSpec.describe '/admin/athletes' do
     before do
       user.admin!
       Bullet.unused_eager_loading_enable = false
+      post batch_action_admin_athletes_url, params: batch_params
     end
 
     after do
@@ -46,23 +47,21 @@ RSpec.describe '/admin/athletes' do
 
     context 'with valid athletes' do
       let(:athletes) { create_list(:athlete, 2, name: 'Same Name', male: true) }
-      let(:valid_attributes) do
+      let(:batch_params) do
         { batch_action: :reunite, batch_action_inputs: '{}', collection_selection: athletes.map(&:id) }
       end
 
       it 'redirects to duplicates scope' do
-        post batch_action_admin_athletes_url, params: valid_attributes
         expect(response).to redirect_to admin_athletes_url(scope: :duplicates)
       end
     end
 
     context 'with invalid attributes' do
-      let(:invalid_attributes) do
+      let(:batch_params) do
         { batch_action: :reunite, batch_action_inputs: {}, collection_selection: [] }
       end
 
       it 'redirects to admin athletes page' do
-        post batch_action_admin_athletes_url, params: invalid_attributes
         expect(response).to redirect_to admin_athletes_url(scope: :duplicates)
       end
     end
@@ -78,7 +77,6 @@ RSpec.describe '/admin/athletes' do
       end
 
       it 'changes gender to all athletes' do
-        post batch_action_admin_athletes_url, params: batch_params
         expect(response).to redirect_to admin_athletes_path
         expect(athletes.map(&:reload)).to all be_male
       end
@@ -86,9 +84,7 @@ RSpec.describe '/admin/athletes' do
   end
 
   describe 'POST /admin/athletes' do
-    before do
-      user.admin!
-    end
+    before { user.admin! }
 
     let(:result) { create(:result, athlete: nil) }
     let(:valid_attributes) do
@@ -105,6 +101,47 @@ RSpec.describe '/admin/athletes' do
         post admin_athletes_url, params: valid_attributes
       end.to change(Athlete, :count).by(1)
       expect(result.reload.athlete).to be_present
+    end
+  end
+
+  describe 'DELETE /admin/athletes/1' do
+    before do
+      user.admin!
+      delete admin_athlete_url(athlete)
+    end
+
+    context 'when athlete is blank' do
+      let(:athlete) { create(:athlete) }
+
+      it 'destroy athlete' do
+        expect(response).to redirect_to admin_athletes_url
+        expect(Athlete.find_by(id: athlete.id)).to be_nil
+      end
+    end
+
+    shared_examples 'not destroy athlete' do
+      it 'not destroy athlete' do
+        expect(response).to redirect_to admin_athletes_url
+        expect(athlete.reload).not_to be_nil
+      end
+    end
+
+    context 'with registered athlete' do
+      let(:athlete) { create(:athlete, user_id: user.id) }
+
+      include_examples 'not destroy athlete'
+    end
+
+    context 'when athlete with results' do
+      let(:athlete) { create(:result).athlete }
+
+      include_examples 'not destroy athlete'
+    end
+
+    context 'when athlete with volunteering' do
+      let(:athlete) { create(:volunteer).athlete }
+
+      include_examples 'not destroy athlete'
     end
   end
 end
