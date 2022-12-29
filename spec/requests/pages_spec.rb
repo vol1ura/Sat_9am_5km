cfec6b583be1fa73736684af42c268186f44052f
Yuriy Volodin <vol1ura@gmail.com>
2022-12-29 23:59:04 +0300

    add tests for request root page

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -10,5 +10,18 @@ RSpec.describe '/pages' do
         expect(response).to be_successful
       end
     end
+
+    context 'when user log in' do
+      before do
+        user = create(:user)
+        create(:athlete, user_id: user.id)
+        sign_in user
+      end
+
+      it 'renders root page with successful response' do
+        get root_url
+        expect(response).to be_successful
+      end
+    end
   end
 end
