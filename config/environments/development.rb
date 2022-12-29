169ac64cc693a51caf67f4fe14de452d47e4d3b3
Yuriy Volodin <vol1ura@gmail.com>
2022-12-29 23:59:04 +0300

    add tests for request root page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{2.days.to_i}"
+      'Cache-Control' => "public, max-age=#{2.hours.to_i}"
     }
   else
     config.action_controller.perform_caching = false
