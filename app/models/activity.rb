aaa0f102223469f8d5ee2d6ea0f2359e5c78fe0c
Yuriy Volodin <vol1ura@gmail.com>
2023-01-10 17:20:40 +0300

    add sorting by volunteering position rank

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -7,15 +7,25 @@ class Activity < ApplicationRecord
 
   has_many :results, dependent: :destroy
   has_many :athletes, through: :results
-  has_many :volunteers, -> { order :id }, dependent: :destroy, inverse_of: :activity
+  has_many :volunteers, dependent: :destroy, inverse_of: :activity
 
   before_save :postprocessing, if: %i[will_save_change_to_published? published]
 
   scope :published, -> { where(published: true) }
   scope :unpublished, -> { where(published: false) }
 
+  def volunteers_roster
+    volunteers
+      .joins("LEFT JOIN volunteering_positions vp on vp.event_id = #{event_id} AND vp.role = volunteers.role")
+      .order(:rank)
+  end
+
   def leader_result(male: true)
     results.joins(:athlete).where(athlete: { male: male }).order(:position).first
+  end
+
+  def number
+    event.activities.where(date: ..date).size
   end
 
   private
