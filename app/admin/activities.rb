b7a0bfade808a186e97224725457111cfc636124
Yuriy Volodin <vol1ura@gmail.com>
2023-01-16 18:06:26 +0300

    add clear cache button for admins

--- app/admin/activities.rb
+++ app/admin/activities.rb
@@ -36,11 +36,24 @@ ActiveAdmin.register Activity do
     flash[:notice] = t('active_admin.activities.success_upload') if params[:activity][:scanner0]
   end
 
+  collection_action :clear_cache, method: :delete, if: proc { current_admin_user.admin? } do
+    if ClearCache.call
+      flash[:notice] = t('active_admin.activities.cache_clear.success')
+    else
+      flash[:alert] = t('active_admin.activities.cache_clear.failed')
+    end
+    redirect_to collection_path
+  end
+
   action_item :results, only: %i[show edit] do
     link_to 'Редактор результатов', admin_activity_results_path(resource)
   end
 
   action_item :results, only: %i[show edit] do
     link_to 'Редактор волонтёров', admin_activity_volunteers_path(resource)
+  end
+
+  action_item :clear_cache, only: :index, if: proc { current_user.admin? } do
+    link_to 'СБРОСИТЬ КЕШ', clear_cache_admin_activities_path, method: :delete
   end
 end
