a3d42e7aec46803177ef207b969fb5ae50644159
Yuriy Volodin <vol1ura@gmail.com>
2023-01-27 22:12:57 +0300

    add funrun badges

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_27_080157) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_27_182935) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
