f2211f3992fcb16cdaaee1d11135e0ce538e7bf7
Yuriy Volodin <vol1ura@gmail.com>
2023-02-13 09:31:57 +0300

    add tests for parkzhrun mailer

--- spec/mailers/parkzhrun_mailer_spec.rb
+++ spec/mailers/parkzhrun_mailer_spec.rb
@@ -1,3 +1,31 @@
 RSpec.describe ParkzhrunMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:recepients) { %w[to1@example.org to2@example.org] }
+
+  before { stub_const('ParkzhrunMailer::RECIPIENTS', recepients) }
+
+  describe '#success' do
+    let(:mail) { described_class.success }
+
+    it 'renders the headers' do
+      expect(mail.subject).to eq('Забег ПаркЖрун создан')
+      expect(mail.to).to eq(recepients)
+    end
+
+    it 'renders the body' do
+      expect(mail.body.encoded).to match('успешно создан')
+    end
+  end
+
+  describe '#error' do
+    let(:mail) { described_class.error }
+
+    it 'renders the headers' do
+      expect(mail.subject).to eq('Ошибка! Не удалось создать забег ПаркЖрун')
+      expect(mail.to).to eq(recepients)
+    end
+
+    it 'renders the body' do
+      expect(mail.body.encoded).to match('произошла ошибка')
+    end
+  end
 end
