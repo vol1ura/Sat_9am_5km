ed18afc8d8d5a3437e8150df47315007f537c7e6
Yuriy Volodin <vol1ura@gmail.com>
2023-02-16 14:09:09 +0300

    add anniversary 50 angarka badge

---
+++ db/migrate/20230216105403_add_anniversary_50_angarka_badge.rb
@@ -0,0 +1,10 @@
+class AddAnniversary50AngarkaBadge < ActiveRecord::Migration[7.0]
+  def change
+    Badge.find_or_create_by!(id: 26) do |badge|
+      badge.name = 'Ангарские пруды 50'
+      badge.conditions = 'Все участники и волонтёры юбилейного 50-ого паркового забега на Ангарских прудах.'
+      badge.picture_link = 'badges/angarka_50.png'
+      badge.received_date = '18.02.2023'
+    end
+  end
+end
