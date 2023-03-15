558c9ccb9adaa0f56a33d8e94eae41fe828888ef
Yuriy Volodin <vol1ura@gmail.com>
2023-03-15 10:06:22 +0300

    add tests for telegram notification services

---
+++ spec/services/telegram/informer/volunteer_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+RSpec.describe Telegram::Informer::Volunteer, type: :service do
+  let(:result) { create(:result, athlete: create(:athlete, user: create(:user))) }
+  let(:bot_token) { '123456:aaabbb' }
+  let!(:request) { stub_request(:post, %r{https://api\.telegram\.org/bot#{bot_token}/sendMessage}) }
+
+  before { stub_const('Telegram::Bot::TOKEN', bot_token) }
+
+  context 'when request to telegram successful' do
+    it 'informs athlete' do
+      described_class.call(result)
+      expect(request).to have_been_requested
+      expect(result.reload).to be_informed
+    end
+  end
+
+  context 'when request to telegram fails' do
+    before { request.to_raise(StandardError) }
+
+    it 'not informs athlete' do
+      described_class.call(result)
+      expect(request).to have_been_requested
+      expect(result.reload).not_to be_informed
+    end
+  end
+end
