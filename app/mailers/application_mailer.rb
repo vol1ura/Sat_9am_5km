bade419f9ad00adc333c6efe7a9973be937c622e
Yuriy Volodin <vol1ura@gmail.com>
2023-05-18 08:51:39 +0300

    move smtp server settings to credentials

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: email_address_with_name(ENV.fetch('EMAIL_FROM'), 'Sat9am5km')
+  default from: email_address_with_name(Rails.application.credentials.mailer.user_name, 'Sat9am5km')
   layout 'mailer'
 end
