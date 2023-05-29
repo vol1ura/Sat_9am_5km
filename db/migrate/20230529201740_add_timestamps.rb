024ddef575e18832c46f31867db9e0ebcd35002d
Yuriy Volodin <vol1ura@gmail.com>
2023-05-30 00:00:25 +0300

    add timestamps

---
+++ db/migrate/20230529201740_add_timestamps.rb
@@ -0,0 +1,10 @@
+class AddTimestamps < ActiveRecord::Migration[7.0]
+  def change
+    current_time = Time.current
+    %i[results trophies volunteers].each do |table|
+      add_timestamps table, default: current_time
+      change_column_default table, :created_at, from: current_time, to: nil
+      change_column_default table, :updated_at, from: current_time, to: nil
+    end
+  end
+end
