2d56dde0aea68a82c8379f907f6f5bd81cbe226b
Yuriy Volodin <vol1ura@gmail.com>
2023-06-01 12:21:29 +0300

    add smf badge

---
+++ db/migrate/20230601091157_add_smf_2023_badge.rb
@@ -0,0 +1,10 @@
+class AddSmf2023Badge < ActiveRecord::Migration[7.0]
+  def change
+    Badge.find_or_create_by!(id: 27) do |badge|
+      badge.name = 'Sport-Marafon fest 2023'
+      badge.conditions = 'Участие в фан-ране на фестивале Спорт-Марафон в качестве бегуна или волонтёра.'
+      badge.picture_link = 'badges/sm_fest_2023.png'
+      badge.received_date = '03.06.2023'
+    end
+  end
+end
