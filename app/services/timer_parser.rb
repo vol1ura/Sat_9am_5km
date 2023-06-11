ae581c93303b9211ca47b9c9179d7121ca5dc6b3
Yuriy Volodin <vol1ura@gmail.com>
2023-06-12 00:07:50 +0300

    disable auditing on results uploading

--- app/services/timer_parser.rb
+++ app/services/timer_parser.rb
@@ -8,17 +8,18 @@ class TimerParser < ApplicationService
 
   def call
     return unless @timer_file
+    raise 'Unknown timer file format' if table.dig(0, 0) != 'STARTOFEVENT'
 
     @activity.transaction do
-      raise 'Unknown timer file format' if table.dig(0, 0) != 'STARTOFEVENT'
-
       @activity.date = Date.parse(table.dig(0, 1)) # Date of event is the second column of first row
       column_correction = 1
-      table[1..].each do |row|
-        break if row.first == 'ENDOFEVENT'
-        column_correction -= 1 and next if row.third.blank? || row.third.include?('00:00:00') # skip lines
+      Result.without_auditing do
+        table[1..].each do |row|
+          break if row.first == 'ENDOFEVENT'
+          column_correction -= 1 and next if row.third.blank? || row.third.include?('00:00:00')
 
-        @activity.results << Result.new(position: row.first.to_i + column_correction, total_time: row.last)
+          @activity.results << Result.new(position: row.first.to_i + column_correction, total_time: row.last)
+        end
       end
       @activity.save!
     end
