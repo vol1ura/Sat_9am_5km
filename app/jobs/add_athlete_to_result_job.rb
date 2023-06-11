1c76d662d8ecf17270f858502234b3feac3bbf05
Yuriy Volodin <vol1ura@gmail.com>
2023-06-12 00:07:50 +0300

    disable auditing on results uploading

--- app/jobs/add_athlete_to_result_job.rb
+++ app/jobs/add_athlete_to_result_job.rb
@@ -6,6 +6,8 @@ class AddAthleteToResultJob < ApplicationJob
   def perform(activity, row)
     athlete = Athlete.find_or_scrape_by_code!(row.first.delete('A').to_i)
     result = activity.results.find_or_initialize_by(position: row.second.delete('P').to_i)
-    result.update!(athlete: athlete)
+    result.without_auditing do
+      result.update!(athlete: athlete)
+    end
   end
 end
