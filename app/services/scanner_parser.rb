1089825a99fdb7f093bbb4d6e0e415dc16f78a5c
Yuriy Volodin <vol1ura@gmail.com>
2023-06-17 12:42:28 +0300

    minor fixes

--- app/services/scanner_parser.rb
+++ app/services/scanner_parser.rb
@@ -11,7 +11,7 @@ class ScannerParser < ApplicationService
     return unless table.dig(1, 0).match?(/A\d+/)
 
     table[1..].each do |row|
-      next unless row.second # Athlete was already scanned
+      next if row.second.blank? # Athlete was already scanned
 
       AddAthleteToResultJob.perform_later(@activity, row)
     end
