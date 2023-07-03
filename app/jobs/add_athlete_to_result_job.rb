07b1d7a1decb26b4a3a5feac88682238bb398e85
Yuriy Volodin <vol1ura@gmail.com>
2023-07-03 10:42:30 +0300

    add discarding invalid record error

--- app/jobs/add_athlete_to_result_job.rb
+++ app/jobs/add_athlete_to_result_job.rb
@@ -3,6 +3,8 @@
 class AddAthleteToResultJob < ApplicationJob
   queue_as :default
 
+  discard_on ActiveRecord::RecordInvalid
+
   def perform(activity, row)
     athlete = Athlete.find_or_scrape_by_code!(row.first.delete('A').to_i)
     result = activity.results.find_or_initialize_by(position: row.second.delete('P').to_i)
