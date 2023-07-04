ca084b56be9715da4cce100cba980fcd1f54da2d
Yuriy Volodin <vol1ura@gmail.com>
2023-07-04 15:21:09 +0300

    add unique index for clubs on name column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_01_153110) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_04_121620) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
@@ -95,6 +95,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_01_153110) do
 
   create_table "clubs", force: :cascade do |t|
     t.string "name", null: false
+    t.index ["name"], name: "index_clubs_on_name", unique: true
   end
 
   create_table "contacts", force: :cascade do |t|
