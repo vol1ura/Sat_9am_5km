381283ba276115d1591abaf46274a7a7f2fea932
Yuriy Volodin <vol1ura@gmail.com>
2023-08-09 12:09:03 +0300

    format dashboard page in admin panel

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,14 +4,28 @@ ActiveAdmin.register_page 'Dashboard' do
   menu priority: 1, label: proc { t 'active_admin.dashboard' }
 
   content title: proc { t 'active_admin.dashboard' } do
-    div class: 'blank_slate_container', id: 'dashboard_default_message' do
-      span class: 'blank_slate' do
-        span t 'active_admin.dashboard_welcome.welcome'
-        small t 'active_admin.dashboard_welcome.notification'
-      end
-    end
-
     columns do
+      column do
+        panel t('active_admin.dashboard_welcome.upcoming_activities') do
+          ul do
+            Activity
+              .includes(:event)
+              .where(date: Date.current..Date.current.end_of_week)
+              .order('event.visible_order')
+              .each do |activity|
+                li link_to_if(can?(:read, activity), human_activity_name(activity), admin_activity_path(activity))
+              end
+          end
+        end
+
+        panel t('active_admin.dashboard_welcome.latest_activities') do
+          ul do
+            Activity.includes(:event).order(created_at: :desc).first(10).each do |activity|
+              li link_to_if(can?(:read, activity), human_activity_name(activity), admin_activity_path(activity))
+            end
+          end
+        end
+      end
       column do
         panel t('active_admin.dashboard_welcome.info') do
           para t 'active_admin.dashboard_welcome.call_to_action'
@@ -46,15 +60,6 @@ ActiveAdmin.register_page 'Dashboard' do
             li 'Увеличен размер шрифтов.'
             li 'Раздел "Волонтёры" перенесён в "Забеги" и доступен из каждого забега по кнопке "Редактор волонтёров".'
             li 'Изменён диалог добавления волонтёрства. Выбор участника может производиться из автокомплита по имени и шк.'
-          end
-        end
-      end
-      column do
-        panel t('active_admin.dashboard_welcome.latest_activities') do
-          ul do
-            Activity.includes(:event).order(created_at: :desc).first(10).each do |activity|
-              li link_to_if(can?(:read, activity), human_activity_name(activity), admin_activity_path(activity))
-            end
           end
         end
       end
--- spec/requests/admin/dashboard_spec.rb
+++ spec/requests/admin/dashboard_spec.rb
@@ -1,13 +1,12 @@
 RSpec.describe '/admin/dashboard' do
   let(:user) { create(:user) }
 
-  before do
-    sign_in user
-  end
+  before { sign_in user }
 
   describe 'GET /admin' do
     it 'renders a successful response' do
       create_list(:activity, 3, published: true)
+      create(:activity, date: Faker::Date.between(from: Date.current, to: Date.current.sunday))
       get admin_dashboard_url
       expect(response).to be_successful
     end
