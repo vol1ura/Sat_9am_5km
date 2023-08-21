23d9d9d03966373ba86db0f4abfd5f01a6f82232
Yuriy Volodin <vol1ura@gmail.com>
2023-08-21 21:27:28 +0300

    add scanner file validations checking

--- app/jobs/add_athlete_to_result_job.rb
+++ app/jobs/add_athlete_to_result_job.rb
@@ -5,9 +5,10 @@ class AddAthleteToResultJob < ApplicationJob
 
   discard_on ActiveRecord::RecordInvalid
 
-  def perform(activity, row)
-    athlete = Athlete.find_or_scrape_by_code!(row.first.delete('A').to_i)
-    result = activity.results.find_or_initialize_by(position: row.second.delete('P').to_i)
+  def perform(activity_id, code, position)
+    activity = Activity.find(activity_id)
+    athlete = Athlete.find_or_scrape_by_code!(code.delete('A').to_i)
+    result = activity.results.find_or_initialize_by(position: position.delete('P').to_i)
     result.without_auditing do
       result.update!(athlete: athlete)
     end
