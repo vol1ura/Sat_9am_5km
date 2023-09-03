e534acfe559714657431c3e8c6bf7906e717c075
Yuriy Volodin <vol1ura@gmail.com>
2023-09-03 13:59:17 +0300

    add test for last week club activities

--- spec/requests/clubs_spec.rb
+++ spec/requests/clubs_spec.rb
@@ -8,13 +8,26 @@ RSpec.describe '/clubs' do
     end
   end
 
-  describe 'GET /show' do
-    it 'renders a successful response' do
-      club = create(:club)
+  context 'with club' do
+    let(:club) { create(:club) }
+
+    before do
       athletes = create_list(:athlete, 3, club: club)
       athletes.each { |athlete| create_list(:result, 3, athlete: athlete) }
-      get club_url(club)
-      expect(response).to be_successful
+    end
+
+    describe 'GET /show' do
+      it 'renders a successful response' do
+        get club_url(club)
+        expect(response).to be_successful
+      end
+    end
+
+    describe 'GET /last_week' do
+      it 'renders a successful response' do
+        get last_week_club_url(club)
+        expect(response).to be_successful
+      end
     end
   end
 end
