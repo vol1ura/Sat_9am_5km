1875e92706112c98f6cb2b008e3f4324c20956fe
Yuriy Volodin <vol1ura@gmail.com>
2023-09-06 09:32:08 +0300

    add ransackable attributes

---
+++ spec/requests/admin/clubs_spec.rb
@@ -0,0 +1,25 @@
+RSpec.describe '/admin/clubs' do
+  let(:user) { create(:user, :admin) }
+
+  before { sign_in user }
+
+  describe 'GET /admin/clubs' do
+    it 'renders a successful response' do
+      create_list(:club, 3)
+      get admin_clubs_url
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'POST /admin/clubs' do
+    let(:valid_attributes) do
+      { club: { name: Faker::Team.name } }
+    end
+
+    it 'creates a new club' do
+      expect do
+        post admin_clubs_url, params: valid_attributes
+      end.to change(Club, :count).by(1)
+    end
+  end
+end
