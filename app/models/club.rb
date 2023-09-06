515d9092d534c6c0794f383816373251f5f83a8f
Yuriy Volodin <vol1ura@gmail.com>
2023-09-06 09:32:08 +0300

    add ransackable attributes

--- app/models/club.rb
+++ app/models/club.rb
@@ -4,4 +4,8 @@ class Club < ApplicationRecord
   has_many :athletes, dependent: :nullify
 
   validates :name, presence: true, uniqueness: true
+
+  def self.ransackable_attributes(_auth_object = nil)
+    ['name']
+  end
 end
