5e8062c3f409955857a89f8dff58bede49db4972
Yuriy Volodin <vol1ura@gmail.com>
2023-10-07 16:17:15 +0300

    add golyanovo first run badge

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_11_145718) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_07_130603) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
