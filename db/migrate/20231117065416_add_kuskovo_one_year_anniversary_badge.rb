4bf0ed1632157457f21ce885f6e460cf80e86ac1
Yuriy Volodin <vol1ura@gmail.com>
2023-11-17 09:59:36 +0300

    add kuskovo one year anniversary badge

---
+++ db/migrate/20231117065416_add_kuskovo_one_year_anniversary_badge.rb
@@ -0,0 +1,14 @@
+class AddKuskovoOneYearAnniversaryBadge < ActiveRecord::Migration[7.0]
+  def up
+    Badge.find_or_create_by!(id: 38) do |badge|
+      badge.name = '#годбегаемвкусково'
+      badge.conditions = 'Волонтёр или участник забега в субботу 9 декабря 2023 года на S95 Кусково. Значок посвящён годовщине парковых пробежек в парке Кусково.'
+      badge.picture_link = 'badges/kuskovo_1_year.png'
+      badge.received_date = '09.12.2023'
+    end
+  end
+
+  def down
+    Badge.find(38).destroy
+  end
+end
