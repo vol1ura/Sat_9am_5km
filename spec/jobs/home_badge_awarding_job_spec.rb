de830a482fb4b57d8f1000b79e4a6d00196ba079
Yuriy Volodin <vol1ura@gmail.com>
2023-12-18 16:02:59 +0300

    add tests for home event badges

--- spec/jobs/home_badge_awarding_job_spec.rb
+++ spec/jobs/home_badge_awarding_job_spec.rb
@@ -1,5 +1,25 @@
 RSpec.describe HomeBadgeAwardingJob do
+  fixtures :badges
+
+  let(:home_event) { create(:event) }
+  let(:athlete) { create(:athlete, event: home_event) }
+
+  before do
+    25.times do |idx|
+      activity = create(:activity, event: home_event, date: idx.week.ago)
+      create(:result, athlete:, activity:)
+      create(:volunteer, athlete:, activity:)
+    end
+  end
+
   it 'performs immediately' do
     expect { described_class.perform_later }.to have_enqueued_job.on_queue('low').at(:no_wait)
+  end
+
+  it 'creates home event trophies' do
+    expect do
+      described_class.perform_now
+    end.to change { athlete.trophies.exists?(badge_id: 39) }.to(true)
+      .and change { athlete.trophies.exists?(badge_id: 42) }.to(true)
   end
 end
