1f42276b59fbf0bd8577089f43a282f6e7e0dd01
Yuriy Volodin <vol1ura@gmail.com>
2023-12-18 17:31:50 +0300

    optimize sql request on home badge awarding for one athlete case

--- app/jobs/home_badge_awarding_job.rb
+++ app/jobs/home_badge_awarding_job.rb
@@ -3,13 +3,15 @@
 class HomeBadgeAwardingJob < ApplicationJob
   queue_as :low
 
-  def perform
+  def perform(athlete_id = nil)
     utmost_badge = Badge.new(info: { threshold: 10**10 })
     Badge::BADGE_TYPES.each do |type|
       badges_dataset = Badge.dataset_of(kind: :home_participating, type: type)
       badges_dataset.to_a.push(utmost_badge).each_cons(2) do |badge, next_badge|
         athlete_ids =
           athlete_ids_ds(type, min_events_count: badge.info['threshold'], max_events_count: next_badge.info['threshold'])
+        athlete_ids = athlete_ids.where(athlete_id:) if athlete_id
+
         Athlete.where(id: athlete_ids).where.not(id: badge.trophies.select(:athlete_id)).find_each do |athlete|
           athlete.trophies.where(badge: badges_dataset.where.not(id: badge.id)).destroy_all
           athlete.trophies.create!(badge: badge, date: date_of_awarding(athlete, badge))
