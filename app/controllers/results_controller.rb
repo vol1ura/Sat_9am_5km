b64d0d308c1bf2c528996e43e8a39d0f2820179d
Yuriy Volodin <vol1ura@gmail.com>
2024-01-04 13:39:15 +0300

    fix country filter

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -5,7 +5,7 @@ class ResultsController < ApplicationController
     scope =
       Result
         .joins(activity: { event: :country })
-        .where(country: { code: domain_locale })
+        .where(country: { code: top_level_domain })
         .includes(athlete: :club, activity: :event)
     @male_results = scope.top(male: true, limit: 50)
     @female_results = scope.top(male: false, limit: 50)
