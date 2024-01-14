4354b1eae0644b3537b8e5ce66035abbc2dea1f5
Yuriy Volodin <vol1ura@gmail.com>
2024-01-14 18:50:20 +0300

    change barcode to QR-code

--- spec/services/barcode_printer_spec.rb
+++ spec/services/barcode_printer_spec.rb
@@ -1,21 +1,8 @@
 # frozen_string_literal: true
 
 RSpec.describe BarcodePrinter, type: :service do
-  context 'with parkrun code' do
-    let(:athlete) { build(:athlete, fiveverst_code: nil) }
-
-    it 'returns svg xml document' do
-      expect(described_class.call(athlete)).to include '<svg xmlns="http://www.w3.org/2000/svg"',
-                                                       "<title>A#{athlete.parkrun_code}</title>"
-    end
-  end
-
-  context 'with 5 verst code' do
-    let(:athlete) { build(:athlete, parkrun_code: nil) }
-
-    it 'returns svg xml document' do
-      expect(described_class.call(athlete)).to include '<svg xmlns="http://www.w3.org/2000/svg"',
-                                                       "<title>A#{athlete.fiveverst_code}</title>"
-    end
+  it 'returns svg xml document' do
+    athlete = build(:athlete)
+    expect(described_class.call(athlete)).to include '<svg version="1.1" xmlns="http://www.w3.org/2000/svg"', '</svg>'
   end
 end
