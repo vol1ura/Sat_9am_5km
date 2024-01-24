60caed2863d6f2ce7442e80f6ddf959dc7ddc886
Yuriy Volodin <vol1ura@gmail.com>
2024-01-24 22:55:37 +0300

    add club images and descriptions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_18_185429) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_23_184414) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
@@ -27,6 +27,34 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_18_185429) do
     t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author"
     t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
     t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource"
+  end
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "activities", force: :cascade do |t|
@@ -98,6 +126,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_18_185429) do
   create_table "clubs", force: :cascade do |t|
     t.string "name", null: false
     t.bigint "country_id", null: false
+    t.text "description"
     t.index "lower((name)::text)", name: "index_clubs_on_lower_name", unique: true
     t.index ["country_id"], name: "index_clubs_on_country_id"
   end
@@ -217,6 +246,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_18_185429) do
     t.index ["athlete_id"], name: "index_volunteers_on_athlete_id"
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "activities", "events"
   add_foreign_key "athletes", "clubs", on_delete: :nullify
   add_foreign_key "athletes", "events", on_delete: :nullify
