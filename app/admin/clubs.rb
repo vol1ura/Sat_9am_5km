dcafe2899967746f4a5b0b540030b3f6c8d1cb3d
Yuriy Volodin <vol1ura@gmail.com>
2024-01-25 18:49:06 +0300

    minor changes

--- app/admin/clubs.rb
+++ app/admin/clubs.rb
@@ -15,6 +15,7 @@ ActiveAdmin.register Club do
     id_column
     column(:logo) { |c| image_tag c.logo.variant(resize_to_limit: [60, 60]) if c.logo.attached? }
     column :name
+    column(:description) { |c| sanitized_text c.description&.truncate(160) }
     column :country
     actions
   end
