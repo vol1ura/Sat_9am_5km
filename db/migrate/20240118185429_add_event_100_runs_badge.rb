29721c49864d037afe5285a799748413d6839fed
Yuriy Volodin <vol1ura@gmail.com>
2024-01-28 20:27:44 +0300

    add awarding by jubilee participating kind badge

---
+++ db/migrate/20240118185429_add_event_100_runs_badge.rb
@@ -0,0 +1,15 @@
+class AddEvent100RunsBadge < ActiveRecord::Migration[7.1]
+  def up
+    Badge.find_or_create_by!(id: 46) do |badge|
+      badge.name = '100-й забег в системе S95'
+      badge.conditions = 'Награда вручается всем участникам и волонтёрам локации, которая зафиксировала сто забегов на платформе S95. Сто суббот поддерживаем друг друга. Бежим дальше!'
+      badge.picture_link = 'badges/event_100_runs.png'
+      badge.kind = :jubilee_participating
+      badge.info = { threshold: 100 }
+    end
+  end
+
+  def down
+    Badge.find(46).destroy
+  end
+end
