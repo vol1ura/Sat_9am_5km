d553f67b4e3ceff7dd219811309dc9aba0b424b5
Yuriy Volodin <vol1ura@gmail.com>
2024-02-05 22:00:35 +0300

    add athletes ratings resource

--- spec/requests/ratings_spec.rb
+++ spec/requests/ratings_spec.rb
@@ -1,6 +1,21 @@
 # frozen_string_literal: true
 
 RSpec.describe 'ratings' do
+  describe 'GET /ratings/athletes' do
+    before do
+      Bullet.n_plus_one_query_enable = false
+      create_list(:result, 3)
+
+      get athletes_ratings_url
+    end
+
+    after do
+      Bullet.n_plus_one_query_enable = true
+    end
+
+    it { expect(response).to be_successful }
+  end
+
   describe 'GET /ratings/results' do
     before do
       activities_list = create_list(:activity, 4)
