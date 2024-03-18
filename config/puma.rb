105d735f7ad98dbfdaa0bad2cc1fa88994d7bba5
Yuriy Volodin <vol1ura@gmail.com>
2024-03-18 11:47:04 +0300

    change puma config

--- config/puma.rb
+++ config/puma.rb
@@ -11,13 +11,9 @@ threads min_threads_count, max_threads_count
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
 # terminating a worker in development environments.
 #
-if ENV.fetch("RAILS_ENV", "development") == "development"
-  worker_timeout 3600
-  # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-  port ENV.fetch("PORT") { 3000 }
-else
-  bind "unix://#{ENV.fetch('APP_DEPLOY_PATH')}/shared/tmp/sockets/s95-puma.sock"
-end
+worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
+# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
+port ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
@@ -26,7 +22,6 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/puma.pid" }
 state_path ENV.fetch("STATEFILE") { "tmp/pids/puma.state" }
-stdout_redirect ENV["ACCESS_LOG"], ENV["ERROR_LOG"], true
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked web server processes. If using threads and workers together
