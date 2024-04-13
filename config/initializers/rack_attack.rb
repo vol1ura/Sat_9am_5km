b33bd61b7e5ba1e95057a6f2e8c94c7acae8ea9a
Yuriy Volodin <vol1ura@gmail.com>
2024-04-13 22:21:16 +0300

    update rack-attack filter params

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -2,12 +2,12 @@ class Rack::Attack
   # Throttle requests by IP
   #
   # Key: "rack::attack:#{Time.now.to_i/:period}:req/ip:#{req.ip}"
-  # Allows 10 requests in 4  seconds
-  #        20 requests in 16 seconds
-  #        30 requests in 64 seconds
-  #        40 requests in ~4 minutes
+  # Allows 10 requests in 3  seconds
+  #        20 requests in 9  seconds
+  #        30 requests in 27 seconds
+  #        40 requests in 81 seconds
   (1..4).each do |level|
-    throttle('req/ip', limit: 10 * level, period: (4**level).seconds) do |req|
+    throttle('req/ip', limit: 10 * level, period: (3**level).seconds) do |req|
       req.ip if req.path.start_with?('/activities') || req.path.start_with?('/athletes')
     end
   end
