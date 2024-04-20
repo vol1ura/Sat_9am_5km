e0a43b054593c13bee580c01d2e7234fb21a7ff5
Yuriy Volodin <vol1ura@gmail.com>
2024-04-20 13:24:13 +0300

    minor changes

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module Sat9am5km
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    # config.autoload_lib(ignore: %w(assets capistrano tasks))
+    config.autoload_lib(ignore: %w[assets capistrano tasks])
 
     # config.eager_load_paths << Rails.root.join("extras")
 
