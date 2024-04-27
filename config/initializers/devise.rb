af0294d25ac4f5da03771669ec491818549b671a
Yuriy Volodin <vol1ura@gmail.com>
2024-04-27 22:58:30 +0300

    tune devise config

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,13 +24,13 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = Rails.application.credentials.mailer.user_name
+  # config.mailer_sender = Rails.application.credentials.mailer.user_name
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
   # Configure the parent class responsible to send e-mails.
-  config.parent_mailer = 'ActionMailer::Base'
+  config.parent_mailer = 'ApplicationMailer'
 
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
