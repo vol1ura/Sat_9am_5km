792a7b2a83aa894539268d935604f72e67906131
Yuriy Volodin <vol1ura@gmail.com>
2024-04-28 16:59:25 +0300

    add trophies raitings

--- app/services/athletes/stats_update.rb
+++ app/services/athletes/stats_update.rb
@@ -7,6 +7,9 @@ module Athletes
     end
 
     def call
+      process_results
+      process_volunteering
+
       @athlete.without_auditing do
         @athlete.update!(stats:)
       end
@@ -15,31 +18,33 @@ module Athletes
     private
 
     def stats
-      data = {}
-      if (results_count = published_results.size).positive?
-        data[:results] = {
-          count: results_count,
-          h_index: h_index(published_results, :event_id),
-          uniq_events: uniq_events(published_results),
-        }
-      end
-      if (volunteering_count = published_volunteering.size).positive?
-        data[:volunteers] = {
-          count: volunteering_count,
-          h_index: h_index(published_volunteering, :role),
-          uniq_events: uniq_events(published_volunteering),
-        }
-      end
-
-      data
+      @stats ||= {}
     end
 
-    def published_results
-      @published_results ||= @athlete.results.published
+    def process_results
+      published_results = @athlete.results.published
+      results_count = published_results.size
+      return if results_count.zero?
+
+      stats[:results] = {
+        count: results_count,
+        h_index: h_index(published_results, :event_id),
+        uniq_events: uniq_events(published_results),
+        trophies: trophies_count,
+      }
     end
 
-    def published_volunteering
-      @published_volunteering ||= @athlete.volunteering.unscope(:order)
+    def process_volunteering
+      published_volunteering = @athlete.volunteering.unscope(:order)
+      volunteering_count = published_volunteering.size
+      return if volunteering_count.zero?
+
+      stats[:volunteers] = {
+        count: volunteering_count,
+        h_index: h_index(published_volunteering, :role),
+        uniq_events: uniq_events(published_volunteering),
+        trophies: trophies_count,
+      }
     end
 
     def uniq_events(dataset)
@@ -48,6 +53,10 @@ module Athletes
 
     def h_index(dataset, param)
       dataset.group(param).order(count_all: :desc).count.values.map.with_index.take_while { |count, idx| count > idx }.size
+    end
+
+    def trophies_count
+      @trophies_count ||= @athlete.trophies.count
     end
   end
 end
