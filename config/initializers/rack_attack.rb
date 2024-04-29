f49b4eb3cd9b67a6efb5713fe7d2636376f605b9
Yuriy Volodin <vol1ura@gmail.com>
2024-04-29 21:27:56 +0300

    add rule

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -8,7 +8,7 @@ class Rack::Attack
   #        40 requests in 81 seconds
   (1..4).each do |level|
     throttle('req/ip', limit: 10 * level, period: (3**level).seconds) do |req|
-      req.ip if req.path.start_with?('/activities') || req.path.start_with?('/athletes')
+      req.ip if req.path.start_with?('/activities') || req.path.start_with?('/athletes') || req.path.start_with?('/user')
     end
   end
 
@@ -16,5 +16,11 @@ class Rack::Attack
     Rack::Attack::Fail2Ban.filter("pentesters-#{req.ip}", maxretry: 3, findtime: 10.minutes, bantime: 1.day) do
       CGI.unescape(req.query_string) =~ %r{/etc/passwd} || req.path.end_with?('.php')
     end
+  end
+
+  blocklist('block pentester logins') do |req|
+    bad_user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) ' \
+      'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'
+    req.path.start_with?('/user') && req.post? && req.user_agent == bad_user_agent
   end
 end
