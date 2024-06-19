2dfe76ca731ae4e97a4d4bba3a5a48f61bd8f22e
Yuriy Volodin <vol1ura@gmail.com>
2024-06-19 20:23:23 +0300

    move images to public/images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -47,7 +47,7 @@ module ApplicationHelper
   end
 
   def event_main_image_tag(event, options = {})
-    image_tag event.main_picture_link || '/events/placeholder_big.jpg', **options
+    image_tag event.main_picture_link || '/images/events/placeholder_big.jpg', **options
   end
 
   def athlete_code_id(athlete)
@@ -70,6 +70,6 @@ module ApplicationHelper
   end
 
   def user_image_url(user)
-    user&.image&.attached? ? user.image.variant(:web) : '/person.jpg'
+    user&.image&.attached? ? user.image.variant(:web) : '/images/person.jpg'
   end
 end
