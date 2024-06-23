1817199e782e7a49f3696fa14d61e60d9eacb3fc
Yuriy Volodin <vol1ura@gmail.com>
2024-06-23 14:03:32 +0300

    setup order of week days in datepicker

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -2,3 +2,12 @@
 //= require activeadmin/quill_editor/quill
 //= require activeadmin/quill_editor_input
 //= require active_admin/searchable_select
+
+$(document).ready(function() {
+  $('.datepicker').datepicker(
+    {
+    firstDay: 1,  // Monday
+    dateFormat: 'yy-mm-dd',
+    showAnim: 'slideDown'
+  });
+});
