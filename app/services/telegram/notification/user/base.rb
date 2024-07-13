f793443412f754a8f226e109bbd92cc5afe6b92f
Yuriy Volodin <vol1ura@gmail.com>
2024-07-13 15:11:42 +0300

    add notification after athletes reuniting

---
+++ app/services/telegram/notification/user/base.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Telegram
+  module Notification
+    module User
+      class Base < Notification::Base
+        def initialize(user)
+          @user = user
+        end
+
+        def call
+          return unless @user&.telegram_id
+
+          notify(@user.telegram_id)
+        rescue StandardError => e
+          Rollbar.error e, user_id: @user.id
+        end
+      end
+    end
+  end
+end
