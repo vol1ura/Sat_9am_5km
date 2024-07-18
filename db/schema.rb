13dd95c6b0e88a0eb2264e9fed34f4c62553033c
Yuriy Volodin <vol1ura@gmail.com>
2024-07-18 22:00:13 +0300

    add telegram login button

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_02_191049) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_19_195147) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
@@ -221,6 +221,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_02_191049) do
     t.string "note"
     t.bigint "telegram_id"
     t.string "telegram_user"
+    t.string "provider"
+    t.string "uid"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
