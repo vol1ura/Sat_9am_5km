0c8ca71ef84e9d0e6110100c2b496873ea0605de
Yuriy Volodin <vol1ura@gmail.com>
2024-08-02 22:50:26 +0300

    improve test coverage

--- spec/requests/admin/contacts_spec.rb
+++ spec/requests/admin/contacts_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe '/admin/contacts' do
     sign_in user
   end
 
-  describe 'GET /admin/contacts' do
+  describe 'GET /admin/events/1/contacts' do
     it 'renders a successful response' do
       create_list(:contact, 2, event:)
       get admin_event_contacts_url(event)
@@ -17,7 +17,15 @@ RSpec.describe '/admin/contacts' do
     end
   end
 
-  describe 'POST /admin/contacts' do
+  describe 'GET /admin/events/1/contacts/1/edit' do
+    it 'renders a successful response' do
+      contact = create(:contact, event:)
+      get edit_admin_event_contact_url(event, contact)
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'POST /admin/events/1/contacts' do
     let(:valid_attributes) do
       {
         contact: {
