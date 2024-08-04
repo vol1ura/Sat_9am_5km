5ad61ea9b1504fe192798974edb8aa6bc8b2b591
Yuriy Volodin <vol1ura@gmail.com>
2024-08-04 08:15:57 +0300

    add test for reunite notification job

--- spec/jobs/telegram/notification/after_reunite_job_spec.rb
+++ spec/jobs/telegram/notification/after_reunite_job_spec.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
 RSpec.describe Telegram::Notification::AfterReuniteJob do
-  it 'performs immediately' do
-    expect { described_class.perform_later(1) }.to have_enqueued_job.on_queue('low').at(:no_wait)
+  specify { expect { described_class.perform_later(1) }.to have_enqueued_job.on_queue('low').at(:no_wait) }
+
+  context 'when runs' do
+    let(:user) { create(:user) }
+
+    before do
+      allow(Telegram::Notification::User::Reunite).to receive(:call)
+      described_class.perform_now(user.id)
+    end
+
+    specify { expect(Telegram::Notification::User::Reunite).to have_received(:call).with(user).once }
   end
 end
