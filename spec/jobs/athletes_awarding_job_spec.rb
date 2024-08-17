99bed5e66a45b1c14567f5cfb67f9f174a92c19b
Yuriy Volodin <vol1ura@gmail.com>
2024-08-17 21:59:16 +0300

    add tests for record badge

--- spec/jobs/athletes_awarding_job_spec.rb
+++ spec/jobs/athletes_awarding_job_spec.rb
@@ -2,11 +2,11 @@
 
 RSpec.describe AthletesAwardingJob do
   let(:athlete) { create(:athlete) }
+  let!(:record_badge) { create(:badge, kind: :record, info: { male: true }) }
 
   before do
     create(:badge, kind: :rage)
     create(:badge, kind: :minute_bingo)
-    create(:badge, kind: :record, info: { male: true })
     create(:badge, kind: :tourist, info: { threshold: 5, type: 'result' })
     create(:badge, kind: :tourist, info: { threshold: 5, type: 'volunteer' })
   end
@@ -28,12 +28,11 @@ RSpec.describe AthletesAwardingJob do
         create(:result, athlete: athlete, activity: activity, total_time: Result.total_time(19, idx))
         create(:volunteer, athlete:, activity:)
       end
+      create(:result, activity: activity, athlete: athlete, total_time: Result.total_time(18, 30))
+      create(:volunteer, activity:, athlete:)
     end
 
     it 'creates new trophies' do
-      create(:result, activity: activity, athlete: athlete, total_time: Result.total_time(18, 30))
-      create(:volunteer, activity:, athlete:)
-
       expect do
         described_class.perform_now(activity.id)
       end.to change(athlete.trophies, :count).by(4)
@@ -41,6 +40,52 @@ RSpec.describe AthletesAwardingJob do
         'record', 'rage', 'participating', 'participating',
       )
       expect(FunrunAwardingJob).to have_received(:perform_later).once
+    end
+  end
+
+  context 'with record badge' do
+    let(:event) { create(:event) }
+    let(:old_best_result) { create(:result, athlete: old_best_result_athlete, total_time: Result.total_time(20, 0)) }
+    let(:old_best_result_athlete) { create(:athlete) }
+    let!(:trophy) do
+      create(
+        :trophy,
+        badge: record_badge,
+        athlete: old_best_result.athlete,
+        info: { data: [{ event_id: event.id, result_id: trophy_result_id }] },
+      )
+    end
+    let(:trophy_result_id) { old_best_result.id }
+
+    before do
+      activity = create(:activity, date: Time.zone.today, event: event)
+      create(:result, activity: activity, athlete: athlete, total_time: Result.total_time(19, 59))
+
+      described_class.perform_now(activity.id)
+    end
+
+    shared_examples 'awards athlete' do
+      specify do
+        expect(athlete.trophies.joins(:badge).pluck(:kind)).to contain_exactly('record')
+        expect(trophy.reload.athlete_id).to eq athlete.id
+      end
+    end
+
+    it 'awards athlete' do
+      expect(athlete.trophies.joins(:badge).pluck(:kind)).to contain_exactly('record')
+      expect(Trophy).not_to exist(id: trophy.id)
+    end
+
+    context 'when athlete updates own best result' do
+      let(:old_best_result_athlete) { athlete }
+
+      it_behaves_like 'awards athlete'
+    end
+
+    context 'with broken result_id in trophy' do
+      let(:trophy_result_id) { 0 }
+
+      it_behaves_like 'awards athlete'
     end
   end
 
