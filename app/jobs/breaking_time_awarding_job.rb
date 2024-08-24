3b286ede52506558b833b1bafce373f4c9c00dd8
Yuriy Volodin <vol1ura@gmail.com>
2024-08-24 16:38:04 +0300

    improve and fix BreakingTimeAwardingJob

--- app/jobs/breaking_time_awarding_job.rb
+++ app/jobs/breaking_time_awarding_job.rb
@@ -5,10 +5,15 @@ class BreakingTimeAwardingJob < ApplicationJob
 
   EXPIRATION_PERIOD = 3.months
 
-  def perform
+  def perform(activity_id = nil)
     expire_badges
+
     [true, false].each do |male|
-      award_badges(Badge.breaking_kind.where("(info->'male')::boolean = ?", male).order(Arel.sql("info->'min'")), male:)
+      award_badges(
+        Badge.breaking_kind.where("(info->'male')::boolean = ?", male).order(Arel.sql("info->'min'")),
+        activity_id:,
+        male:,
+      )
     end
   end
 
@@ -17,41 +22,46 @@ class BreakingTimeAwardingJob < ApplicationJob
   def expire_badges
     Badge.breaking_kind.each do |badge|
       accomplished_athlete_ids =
-        Result
-          .joins(:activity, athlete: :trophies)
-          .where(activity: { date: EXPIRATION_PERIOD.ago.. })
+        results_dataset
+          .joins(athlete: :trophies)
           .where(total_time: ...minutes_threshold(badge.info['min']))
-          .where(athlete: { trophies: { badge: } }).select(:athlete_id)
+          .where(athlete: { trophies: { badge: } })
+          .select(:athlete_id)
       Trophy.where(badge:).where.not(athlete_id: accomplished_athlete_ids).destroy_all
     end
   end
 
   # rubocop:disable Metrics/MethodLength
-  def award_badges(badges, male:)
+  def award_badges(badges, activity_id:, male:)
     badges.each do |badge|
       time_threshold = badge.info['min']
       prev_badges = badges.where("(info->'min')::integer < ?", time_threshold)
       next_badges = badges.where("(info->'min')::integer > ?", time_threshold)
-      Result
-        .joins(:activity, :athlete)
-        .where(activity: { date: EXPIRATION_PERIOD.ago.. })
+      results_dataset(activity_id:)
+        .joins(:athlete)
         .where(total_time: minutes_threshold(prev_badges.last&.info&.dig('min'))...minutes_threshold(time_threshold))
         .where(athlete: { male: })
         .order(:date)
         .select(:athlete_id, :date)
         .each do |res|
-          next if Trophy.exists?(athlete_id: res.athlete_id, badge: prev_badges)
+          next if Trophy.exists?(athlete_id: res[:athlete_id], badge: prev_badges)
 
-          trophy = Trophy.find_or_initialize_by(athlete_id: res.athlete_id, badge: badge)
+          trophy = Trophy.find_or_initialize_by(athlete_id: res[:athlete_id], badge: badge)
           trophy.date = res[:date]
           trophy.transaction do
-            Trophy.where(athlete_id: res.athlete_id, badge: next_badges).delete_all
+            Trophy.where(athlete_id: res[:athlete_id], badge: next_badges).delete_all
             trophy.save!
           end
         end
     end
   end
   # rubocop:enable Metrics/MethodLength
+
+  def results_dataset(activity_id: nil)
+    ds = Result.published.where(activity: { date: EXPIRATION_PERIOD.ago.. })
+    ds = ds.where(activity_id:) if activity_id
+    ds
+  end
 
   # Threshold time. It is 00:00:00 for the first interval.
   def minutes_threshold(minutes)
