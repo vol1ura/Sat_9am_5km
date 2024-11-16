19cca5a4182ebbe274638e011eeed09099e70a52
Yuriy Volodin <vol1ura@gmail.com>
2024-11-16 23:05:15 +0300

    bump rails_performance to 1.3.3

--- config/initializers/rails_performance.rb
+++ config/initializers/rails_performance.rb
@@ -1,10 +1,7 @@
 RailsPerformance.setup do |config|
   config.enabled = ENV['RAILS_PERFORMANCE_ENABLED'] == 'true'
 
-  config.redis = Redis::Namespace.new(
-    "#{Rails.env}-rails-performance",
-    redis: Redis.new(host: ENV.fetch('REDIS_HOST', '127.0.0.1')),
-    )
+  config.redis = Redis.new(url: ENV['REDIS_URL'].presence || 'redis://127.0.0.1:6379/2')
 
   config.http_basic_authentication_enabled = false
 
