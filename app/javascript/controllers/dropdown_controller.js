eafb2a5a85ce1bff15774493841d491997c2866a
Yuriy Volodin <vol1ura@gmail.com>
2024-12-16 15:08:43 +0300

    small changes in js controllers

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -1,25 +1,25 @@
-import { Controller } from "@hotwired/stimulus";
+import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-  static targets = ["toggle", "menu"]
+  static targets = ['toggle', 'menu']
 
   connect() {
-    this.element.addEventListener("mouseenter", this.show.bind(this))
-    this.element.addEventListener("mouseleave", this.hide.bind(this))
+    this.element.addEventListener('mouseenter', this.show.bind(this))
+    this.element.addEventListener('mouseleave', this.hide.bind(this))
   }
 
   disconnect() {
-    this.element.removeEventListener("mouseenter", this.show.bind(this))
-    this.element.removeEventListener("mouseleave", this.hide.bind(this))
+    this.element.removeEventListener('mouseenter', this.show.bind(this))
+    this.element.removeEventListener('mouseleave', this.hide.bind(this))
   }
 
   show() {
-    this.toggleTarget.setAttribute("aria-expanded", "true")
-    this.menuTarget.classList.add("show")
+    this.toggleTarget.setAttribute('aria-expanded', 'true')
+    this.menuTarget.classList.add('show')
   }
 
   hide() {
-    this.toggleTarget.setAttribute("aria-expanded", "false")
-    this.menuTarget.classList.remove("show")
+    this.toggleTarget.setAttribute('aria-expanded', 'false')
+    this.menuTarget.classList.remove('show')
   }
 }
--- app/javascript/controllers/protocol_controller.js
+++ app/javascript/controllers/protocol_controller.js
@@ -1,16 +1,16 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  static targets = ["absolute", "male", "female"]
+  static targets = ['absolute', 'male', 'female']
 
   initialize() {
     this.maleTarget.innerHTML = this.absoluteTarget.innerHTML
-    this.#filterTableByMale(this.maleTarget, 'true')
+    this.#filterTable(this.maleTarget, 'true')
     this.femaleTarget.innerHTML = this.absoluteTarget.innerHTML
-    this.#filterTableByMale(this.femaleTarget, 'false')
+    this.#filterTable(this.femaleTarget, 'false')
   }
 
-  #filterTableByMale(table, is_male) {
+  #filterTable(table, is_male) {
     let new_position = 0
     table.querySelectorAll('tbody tr').forEach(row => {
       if (row.dataset.male !== is_male) {
