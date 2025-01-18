684139a37827c7a782b265c7e076c4f00125679a
Yuriy Volodin <vol1ura@gmail.com>
2025-01-18 15:30:45 +0300

    add history stats to info mobile api endpoint

--- app/controllers/api/mobile/athletes_controller.rb
+++ app/controllers/api/mobile/athletes_controller.rb
@@ -5,7 +5,19 @@ module API
     class AthletesController < ApplicationController
       before_action :find_athlete
 
-      def info; end
+      def info
+        @history_stats =
+          @athlete
+            .volunteering
+            .where(activity: { date: 5.months.ago.beginning_of_month.. })
+            .unscope(:order)
+            .group(:role, Arel.sql("date_part('month', date)"))
+            .count
+            .each_with_object({}) do |(k, v), h|
+              h[k.first] ||= Array.new(6, 0)
+              h[k.first][5 - ((Date.current.mon - k.last) % 12)] = v
+            end
+      end
 
       private
 
