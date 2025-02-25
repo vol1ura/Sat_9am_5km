b4cf9003c2d5d733b46d50407a24f4212cb365be
Yuriy Volodin <vol1ura@gmail.com>
2025-02-25 23:32:03 +0300

    Add closest events info on the main page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,27 @@
 # frozen_string_literal: true
 
 class EventsController < ApplicationController
-  before_action :find_event, except: :search
+  before_action :find_event, except: %i[index search]
+
+  def index
+    @local_events = Event.in_country(top_level_domain).unscope(:order)
+    @next_saturday = Date.tomorrow.next_week.prev_week(:saturday)
+    @jubilee_events =
+      Activity
+        .where(event: @local_events.without_friends, published: true, date: ...@next_saturday)
+        .group(:event).count.filter_map do |event, activities_count|
+          activity_number = activities_count.next
+          next unless (activity_number <= 40 && (activity_number % 10).zero?) || (activity_number % 50).zero?
+
+          [event, activity_number]
+        end
+    @funrun_badges =
+      Badge
+        .includes(:image_attachment)
+        .funrun_kind
+        .where(received_date: @next_saturday)
+        .where("info->>'country_code' IS NULL OR info->>'country_code' = ?", top_level_domain)
+  end
 
   def search
     @events =
