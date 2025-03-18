2542ab9a387ee73c18791be1027f4e56a46feba9
Yuriy Volodin <vol1ura@gmail.com>
2025-03-18 21:50:05 +0300

    add validation on trophies if athlete already has such kind

--- app/models/trophy.rb
+++ app/models/trophy.rb
@@ -1,10 +1,23 @@
 # frozen_string_literal: true
 
 class Trophy < ApplicationRecord
+  PARTICIPATING_KINDS = %w[participating home_participating tourist].freeze
+
   belongs_to :badge
   belongs_to :athlete
 
   validates :athlete_id, uniqueness: { scope: :badge_id }
+  validate :threshold_awards_uniqueness
 
   store_accessor :info, :data
+
+  private
+
+  def threshold_awards_uniqueness
+    return if badge_id.nil? || athlete_id.nil? || PARTICIPATING_KINDS.exclude?(badge.kind)
+
+    other_trophies =
+      athlete.trophies.excluding(self).where(badge: Badge.dataset_of(kind: badge.kind, type: badge.info['type']))
+    errors.add(:athlete, :more_than_one_trophy) if other_trophies.exists?
+  end
 end
