134e934305fc18bdca60321b435e328500961ffd
Yuriy Volodin <vol1ura@gmail.com>
2025-03-26 20:55:55 +0300

    set going to event for next occuring saturday

--- app/jobs/reset_going_athletes_job.rb
+++ app/jobs/reset_going_athletes_job.rb
@@ -7,5 +7,13 @@ class ResetGoingAthletesJob < ApplicationJob
     Athlete
       .where.not(going_to_event_id: nil)
       .update_all(going_to_event_id: nil) # rubocop:disable Rails/SkipsModelValidations
+
+    Volunteer
+      .eager_load(:activity)
+      .includes(:athlete)
+      .where(activities: { date: Date.current.next_occurring(:saturday) })
+      .find_each do |volunteer|
+        volunteer.athlete.update(going_to_event_id: volunteer.activity.event_id)
+      end
   end
 end
