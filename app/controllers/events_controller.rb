8c60a7b137e1fe75f3dbd9a8d9132a93cc99d50e
Yuriy Volodin <vol1ura@gmail.com>
2025-04-05 15:07:30 +0300

    extract index page and move map to events index

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,26 +1,11 @@
 # frozen_string_literal: true
 
 class EventsController < ApplicationController
-  before_action :find_event, except: %i[index search map]
+  before_action :find_event, except: %i[index search]
 
   def index
-    @local_events = Event.in_country(top_level_domain).unscope(:order)
-    @next_saturday = Date.tomorrow.next_week.prev_week(:saturday)
-    @jubilee_events =
-      Activity
-        .where(event: @local_events.without_friends, published: true, date: ...@next_saturday)
-        .group(:event).count.filter_map do |event, activities_count|
-          activity_number = activities_count.next
-          next unless (activity_number <= 40 && (activity_number % 10).zero?) || (activity_number % 50).zero?
-
-          [event, activity_number]
-        end
-    @funrun_badges =
-      Badge
-        .includes(:image_attachment)
-        .funrun_kind
-        .where(received_date: @next_saturday)
-        .where("info->>'country_code' IS NULL OR info->>'country_code' = ?", top_level_domain)
+    @events = Event.all
+    @events = @events.in_country(top_level_domain) unless params[:all]
   end
 
   def search
@@ -51,15 +36,6 @@ class EventsController < ApplicationController
 
     activity_id = params[:activity_id] || @activities.first&.id
     @activity = event_future_activities_dataset.find_by(id: activity_id) if activity_id
-  end
-
-  def map
-    @events = Event.where('latitude IS NOT NULL AND longitude IS NOT NULL').map do |event|
-      event.as_json(only: %i[active name slogan code_name]).merge(
-        latitude: event.latitude.to_f,
-        longitude: event.longitude.to_f,
-      )
-    end
   end
 
   private
---
+++ app/views/pages/index.json.jbuilder
@@ -0,0 +1,5 @@
+sql = 'exists (select 1 from activities where activities.event_id = events.id and activities.published = true)'
+json.events @local_events.where(sql) do |event|
+  json.extract! event, :active, :town, :name, :place
+  json.url event_url(event.code_name, format: :json)
+end
