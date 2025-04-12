506dec4808d6ca0d18ce8ab4d46e9ecf395cbc2a
Yuriy Volodin <vol1ura@gmail.com>
2025-04-12 15:33:29 +0300

    add serbian locale for apexcharts

--- app/javascript/controllers/athlete_controller.js
+++ app/javascript/controllers/athlete_controller.js
@@ -2,13 +2,17 @@ import { Controller } from '@hotwired/stimulus'
 import ApexCharts from 'apexcharts'
 import AthleteCharts from 'charts/athlete'
 import { ruLocale } from 'charts/ru'
+import { rsLocale } from 'charts/rs'
 
 // Connects to data-controller="athlete"
 export default class extends Controller {
   static targets = ['results', 'eventsCount', 'eventsWhiskers']
 
   connect() {
-    Apex.chart = { locales: [ruLocale], defaultLocale: 'ru' }
+    Apex.chart = {
+      locales: [ruLocale, rsLocale],
+      defaultLocale: document.documentElement.lang === 'rs' ? 'rs' : 'ru',
+    }
     const athleteCharts = new AthleteCharts(document.querySelectorAll('tr.result'))
 
     if (this.hasResultsTarget) {
