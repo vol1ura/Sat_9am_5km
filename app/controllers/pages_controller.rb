21703aa16a14d07d94bb8f05103a5ef405cb9070
Yuriy Volodin <vol1ura@gmail.com>
2025-05-31 13:07:14 +0300

    remove novi-sad-100 page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class PagesController < ApplicationController
-  ALLOWED_PAGES = %w[about rules support additional-events privacy-policy robots 5za5 novi-sad-100].freeze
+  ALLOWED_PAGES = %w[about rules support additional-events privacy-policy robots 5za5].freeze
 
   before_action :validate_page, except: %i[index submit_feedback]
 
