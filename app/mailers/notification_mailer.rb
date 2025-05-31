ca0cb0c8454367a9e6a2a20030fbc28780a3a409
Yuriy Volodin <vol1ura@gmail.com>
2025-05-31 21:31:53 +0300

    change recipients for feedback mails

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
 class NotificationMailer < ApplicationMailer
+  RECIPIENTS = [ENV.fetch('ADMIN_EMAIL'), ENV.fetch('INFO_EMAIL')].freeze
+
   def parkzhrun_error
-    mail(to: [ENV.fetch('ADMIN_EMAIL'), ENV.fetch('INFO_EMAIL')], subject: t('.parkzhrun_error'))
+    mail(to: RECIPIENTS, subject: t('.parkzhrun_error'))
   end
 
   def feedback
     @message = params[:message]
     @user_id = params[:user_id]
-    mail(to: ENV.fetch('ADMIN_EMAIL'), subject: t('.feedback'))
+    mail(to: RECIPIENTS, subject: t('.feedback'))
   end
 end
