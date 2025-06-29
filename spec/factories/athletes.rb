eadc2c94b382ff4332c7edc8c7d8ceb5a44d3450
Yuriy Volodin <vol1ura@gmail.com>
2025-06-29 22:33:25 +0300

    add tests for volunteers notification job

--- spec/factories/athletes.rb
+++ spec/factories/athletes.rb
@@ -6,5 +6,9 @@ FactoryBot.define do
     parkrun_code { Faker::Number.number(digits: 6) }
     fiveverst_code { Faker::Number.between(from: Athlete::FIVE_VERST_BORDER, to: Athlete::FIVE_VERST_BORDER + (10**7) - 1) }
     male { true }
+
+    trait :with_user do
+      user
+    end
   end
 end
