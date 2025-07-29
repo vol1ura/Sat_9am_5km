bf9392dca23807aa8c1109c49e2a1aa2f0d906ab
Yuriy Volodin <vol1ura@gmail.com>
2025-07-29 22:17:25 +0300

    add carousel for app features

---
+++ app/javascript/controllers/carousel_controller.js
@@ -0,0 +1,34 @@
+import { Controller } from '@hotwired/stimulus'
+
+export default class extends Controller {
+  static targets = ['image', 'feature']
+
+  connect() {
+    this.currentIndex = 0
+  }
+
+  updateDisplay() {
+    this.imageTarget.style.opacity = '0.5'
+
+    setTimeout(() => {
+      this.imageTarget.src = `/images/mobile_app_${this.currentIndex + 1}.png`
+      this.imageTarget.style.opacity = '1'
+
+      this.featureTargets.forEach((feature, index) => {
+        if (index === this.currentIndex) {
+          feature.classList.add('feature-active')
+        } else {
+          feature.classList.remove('feature-active')
+        }
+      })
+    }, 250)
+  }
+
+  showSlide(event) {
+    const index = parseInt(event.currentTarget.dataset.index)
+    if (index === this.currentIndex) return
+
+    this.currentIndex = index
+    this.updateDisplay()
+  }
+}
