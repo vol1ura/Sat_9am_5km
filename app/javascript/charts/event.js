7d0c43c8138dac2f188614051589014801d221cc
Yuriy Volodin <vol1ura@gmail.com>
2025-08-17 14:18:06 +0300

    refactor events diagrams

--- app/javascript/charts/event.js
+++ app/javascript/charts/event.js
@@ -1,22 +1,55 @@
+import ApexCharts from 'apexcharts'
+import { ruLocale } from 'charts/ru'
+import { rsLocale } from 'charts/rs'
+
+const translations = {
+  ru: {
+    participants: 'Участники',
+    volunteers: 'Волонтёры',
+    count: 'Количество',
+  },
+  rs: {
+    participants: 'Učesnici',
+    volunteers: 'Vlontori',
+    count: 'Broj',
+  },
+}
+
 export default class EventCharts {
   constructor(rows) {
-    this._rows = rows;
-    this._eventsData = {};
+    this.rows = rows;
+    this.eventsData = {};
+    this.currentLocale = document.documentElement.lang === 'rs' ? 'rs' : 'ru'
+    this.t = translations[this.currentLocale]
+
+    Apex.chart = { locales: [ruLocale, rsLocale], defaultLocale: this.currentLocale }
+  }
+
+  initializeCharts(resultsTarget, volunteersTarget) {
+    if (resultsTarget) {
+      const chart = new ApexCharts(resultsTarget, this.#chartOptions('results-count-chart', this.t.participants, 'results_count'))
+      chart.render()
+    }
+
+    if (volunteersTarget) {
+      const chart = new ApexCharts(volunteersTarget, this.#chartOptions('volunteers-count-chart', this.t.volunteers, 'volunteers_count'))
+      chart.render()
+    }
   }
 
   get #eventsData() {
-    if (Object.keys(this._eventsData).length === 0) {
-      this._rows.forEach(row => {
-        this._eventsData[row.querySelector('td.date').dataset.date] = {
+    if (Object.keys(this.eventsData).length === 0) {
+      this.rows.forEach(row => {
+        this.eventsData[row.querySelector('td.date').dataset.date] = {
           results_count: Number(row.querySelector('td.results_count').textContent),
           volunteers_count: Number(row.querySelector('td.volunteers_count').textContent),
         };
       });
     }
-    return this._eventsData;
+    return this.eventsData;
   }
 
-  chartOptions(chartId, title, valueClass) {
+  #chartOptions(chartId, title, valueClass) {
     return {
       chart: {
         id: chartId,
@@ -34,7 +67,7 @@ export default class EventCharts {
         opacity: 1,
       },
       series: [{
-        name: 'Количество',
+        name: this.t.count,
         data: Object.values(this.#eventsData).map(data => data[valueClass])
       }],
       labels: Object.keys(this.#eventsData),
