959dc65287b3cbae47c90afa99dd258fd23f825d
Yuriy Volodin <vol1ura@gmail.com>
2025-08-30 23:01:00 +0300

    update rack-attack rules

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,4 +1,5 @@
 class Rack::Attack
+  FRAUD_PATTERNS_REGEXP = Regexp.union(%w[etc/passwd ../../ .php .env]).freeze
   BLACKLIST_IPS = ENV['BLACKLIST_IPS'].to_s.split(',').freeze
 
   # Throttle requests by IP
@@ -17,18 +18,19 @@ class Rack::Attack
     end
   end
 
-  throttle("athletes/ip", limit: 50, period: 30.seconds) do |req|
+  throttle('athletes/ip', limit: 50, period: 30.seconds) do |req|
     req.ip if req.path.match?(%r{^/athletes[^/]})
   end
 
-  throttle("auth_links/ip", limit: 10, period: 1.minute) do |req|
+  throttle('auth_links/ip', limit: 10, period: 1.minute) do |req|
     req.ip if req.path.start_with?('/auth_links/')
   end
 
+  throttle('pentesters/ip', limit: 2, period: 5.minutes) do |req|
+    req.ip if CGI.unescape(req.path).match?(FRAUD_PATTERNS_REGEXP)
+  end
+
   blocklist('pentesters block') do |req|
-    path = CGI.unescape(req.path)
-    %w[etc/passwd ../../ .php].any? { |pattern| path.include?(pattern) } ||
-      req.env['HTTP_ACCEPT'].to_s.include?('../../') ||
-      BLACKLIST_IPS.include?(req.ip)
+    req.env['HTTP_ACCEPT'].to_s.include?('../../') || BLACKLIST_IPS.include?(req.ip)
   end
 end
