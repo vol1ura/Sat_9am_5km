6513260ec1d328bd91591c1afe98258de8937b6d
Yuriy Volodin <vol1ura@gmail.com>
2025-09-09 10:58:14 +0300

    add sorting by comment to volunteers roster

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -28,7 +28,7 @@ class Activity < ApplicationRecord
   def volunteers_roster
     volunteers
       .joins("LEFT JOIN volunteering_positions vp ON vp.event_id = #{event_id} AND vp.role = volunteers.role")
-      .order(:rank, :role)
+      .order(:rank, :role, :comment)
   end
 
   def participants
