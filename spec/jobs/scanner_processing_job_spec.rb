91d032a6e9b9ac8b63f587b76e918bcf17b9f31c
Yuriy Volodin <vol1ura@gmail.com>
2025-10-06 21:25:43 +0300

    add test for handling incorrect results in ScannerProcessingJob

--- spec/jobs/scanner_processing_job_spec.rb
+++ spec/jobs/scanner_processing_job_spec.rb
@@ -24,4 +24,17 @@ RSpec.describe ScannerProcessingJob do
       expect(activity.results.pluck(:athlete_id)).to include(athlete.id, result_athlete.id)
     end
   end
+
+  context 'with incorrect result' do
+    let(:result) { build(:result, activity: activity, athlete: athlete, position: other_result.position + 1) }
+    let(:other_result) { create(:result, activity:, athlete:) }
+
+    it 'alerts about incorrect result' do
+      expect(Telegram::Notification::ActivityAlertJob).to have_been_enqueued.with(
+        activity.id,
+        %w[director results_handler],
+        include("проверьте позицию *P#{result.position}*, а также участника с ID *A#{athlete.code}* на дублирование"),
+      )
+    end
+  end
 end
