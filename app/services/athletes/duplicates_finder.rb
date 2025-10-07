eeaeb74c60a4b201dba48a2e56895c79d9257247
Yuriy Volodin <vol1ura@gmail.com>
2025-10-07 21:20:52 +0300

    enhance duplicates finder to include event count in duplicate detection logic

--- app/services/athletes/duplicates_finder.rb
+++ app/services/athletes/duplicates_finder.rb
@@ -12,6 +12,7 @@ module Athletes
         .from(grouped_names_query)
         .where('name_cnt > 1')
         .where('user_cnt < name_cnt')
+        .where('event_cnt < name_cnt')
         .where('parkrun_cnt < name_cnt')
         .where('fiveverst_cnt < name_cnt')
         .where('runpark_cnt < name_cnt')
@@ -26,34 +27,40 @@ module Athletes
           .select(
             :id,
             :user_id,
+            :event_id,
             :parkrun_code,
             :fiveverst_code,
             :runpark_code,
             Arel.sql("string_agg(words, ' ' ORDER BY words) AS normalized_name"),
           )
           .from(Arel.sql("athletes, unnest(string_to_array(LOWER(name), ' ')) AS words"))
-          .group(:id, :user_id, :parkrun_code, :fiveverst_code, :runpark_code)
+          .group(:id, :user_id, :event_id, :parkrun_code, :fiveverst_code, :runpark_code)
 
       ds = ds.having("string_agg(words, ' ' ORDER BY words) = ?", @name.downcase.split.sort.join(' ')) if @name
       ds
     end
 
     def grouped_names_query
-      grouped_names = Arel::Table.new(:sorted_names)
       grouped_names
         .project(
           grouped_names[:id],
           grouped_names[:user_id],
+          grouped_names[:event_id],
           grouped_names[:parkrun_code],
           grouped_names[:fiveverst_code],
           grouped_names[:runpark_code],
           grouped_names[:normalized_name],
           Arel.sql('COUNT(*) OVER (PARTITION BY normalized_name) AS name_cnt'),
           Arel.sql('COUNT(user_id) OVER (PARTITION BY normalized_name) AS user_cnt'),
+          Arel.sql('COUNT(event_id) OVER (PARTITION BY normalized_name) AS event_cnt'),
           Arel.sql('COUNT(parkrun_code) OVER (PARTITION BY normalized_name) AS parkrun_cnt'),
           Arel.sql('COUNT(fiveverst_code) OVER (PARTITION BY normalized_name) AS fiveverst_cnt'),
           Arel.sql('COUNT(runpark_code) OVER (PARTITION BY normalized_name) AS runpark_cnt'),
         ).as('grouped_names')
+    end
+
+    def grouped_names
+      @grouped_names ||= Arel::Table.new(:sorted_names)
     end
   end
 end
