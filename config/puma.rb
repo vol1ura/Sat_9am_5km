89bc297c9dde561d8a56e8cdcf89db2fe1d35e63
Yuriy Volodin <vol1ura@gmail.com>
2025-10-08 08:34:21 +0300

    tune capistrano config

--- config/puma.rb
+++ config/puma.rb
@@ -27,8 +27,20 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
 
+# Specifies the number of `workers` to boot in clustered mode.
+# Workers are forked web server processes. If using threads and workers together
+# the concurrency of the application would be max `threads` * `workers`.
+# Workers do not work on JRuby or Windows (both of which do not support processes).
+# workers ENV.fetch("WEB_CONCURRENCY", 2)
+
+# Use the `preload_app!` method when specifying a `workers` number.
+# This directive tells Puma to first boot the application and load code
+# before forking the application. This takes advantage of Copy On Write
+# process behavior so workers use less memory.
+# preload_app!
+
 if ENV["RAILS_ENV"] == "production"
-  bind ENV["PUMA_BIND"]
+  bind ENV.fetch("PUMA_BIND", "unix://#{Dir.pwd}/tmp/sockets/puma.sock")
 else
   port ENV.fetch("PORT", 3000)
 end
