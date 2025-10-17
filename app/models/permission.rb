b14297bb1f0dd5e1b437a44c8dcc4d36951d4f79
Yuriy Volodin <vol1ura@gmail.com>
2025-10-17 20:09:58 +0300

    add Club to the list of permission classes

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -2,7 +2,7 @@
 
 class Permission < ApplicationRecord
   ACTIONS = %w[read create update destroy manage].freeze
-  CLASSES = %w[Activity Volunteer Athlete Result VolunteeringPosition].freeze
+  CLASSES = %w[Activity Volunteer Athlete Result VolunteeringPosition Club].freeze
 
   belongs_to :user
   belongs_to :event, optional: true
