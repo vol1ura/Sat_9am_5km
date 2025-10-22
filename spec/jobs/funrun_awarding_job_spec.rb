70f6f14ddf49a051afb40ddbdd309432bf24ed4e
Yuriy Volodin <vol1ura@gmail.com>
2025-10-22 22:55:08 +0300

    add test for FunrunAwardingJob

--- spec/jobs/funrun_awarding_job_spec.rb
+++ spec/jobs/funrun_awarding_job_spec.rb
@@ -4,29 +4,50 @@ RSpec.describe FunrunAwardingJob do
   let(:activity) { create(:activity) }
   let(:athlete) { create(:athlete) }
   let(:volunteer) { create(:athlete) }
-  let(:badge) { create(:badge, kind: :funrun, received_date: activity.date) }
+  let(:badge) { create(:badge, kind: :funrun, received_date: received_date) }
+  let(:received_date) { activity.date }
 
   before do
     create(:result, activity:, athlete:)
     create(:volunteer, activity: activity, athlete: volunteer)
   end
 
-  it 'awards athlete by badge' do
-    expect do
-      described_class.perform_now(activity.id, badge.id)
+  shared_examples 'awarding' do
+    it 'awards athlete by badge' do
+      expect do
+        described_class.perform_now(activity.id, badge.id)
+      end
+        .to change { athlete.trophies.exists?(badge:) }.from(false).to(true)
+        .and change { volunteer.trophies.exists?(badge:) }.from(false).to(true)
     end
-      .to change { athlete.trophies.exists?(badge:) }.from(false).to(true)
-      .and change { volunteer.trophies.exists?(badge:) }.from(false).to(true)
   end
 
-  context 'when athlete already has this badge' do
-    before { create(:trophy, badge:, athlete:) }
-
+  shared_examples 'no awarding' do
     it 'does not award athlete' do
       expect do
         described_class.perform_now(activity.id, badge.id)
       end
         .not_to(change { athlete.reload.trophies.count })
     end
+  end
+
+  it_behaves_like 'awarding'
+
+  context 'with jubilee badge' do
+    let(:badge) { create(:badge, kind: :jubilee_participating, info: { threshold: activity.number }) }
+
+    it_behaves_like 'awarding'
+  end
+
+  context 'when athlete already has this badge' do
+    before { create(:trophy, badge:, athlete:) }
+
+    it_behaves_like 'no awarding'
+  end
+
+  context 'when funrun badge date is mismatched with activity date' do
+    let(:received_date) { activity.date - 1.day }
+
+    it_behaves_like 'no awarding'
   end
 end
