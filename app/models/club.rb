3342fe7de2444b132e2055789e19c9e22c068e5b
Yuriy Volodin <vol1ura@gmail.com>
2025-11-04 23:09:07 +0300

    add ransack for club search

--- app/models/club.rb
+++ app/models/club.rb
@@ -13,8 +13,42 @@ class Club < ApplicationRecord
   validates :logo, size: { less_than: 250.kilobytes }, dimension: { width: { in: 150..900 }, height: { in: 150..900 } }
 
   def self.ransackable_attributes(_auth_object = nil)
-    %w[name country_id]
+    %w[name country_id athletes_count results_count volunteering_count]
   end
 
   def to_combobox_display = name
+
+  ransacker :athletes_count do
+    Arel.sql(
+      '(
+        SELECT COUNT(athletes.id)
+        FROM athletes
+        WHERE athletes.club_id = clubs.id
+      )',
+    )
+  end
+
+  ransacker :results_count do
+    Arel.sql(
+      '(
+        SELECT COUNT(results.id)
+        FROM results
+        INNER JOIN athletes ON athletes.id = results.athlete_id
+        INNER JOIN activities ON activities.id = results.activity_id
+        WHERE athletes.club_id = clubs.id AND activities.published = TRUE
+      )',
+    )
+  end
+
+  ransacker :volunteering_count do
+    Arel.sql(
+      '(
+        SELECT COUNT(volunteers.id)
+        FROM volunteers
+        INNER JOIN athletes ON athletes.id = volunteers.athlete_id
+        INNER JOIN activities ON activities.id = volunteers.activity_id
+        WHERE athletes.club_id = clubs.id AND activities.published = TRUE
+      )',
+    )
+  end
 end
