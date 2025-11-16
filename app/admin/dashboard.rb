1b1eb872ccf7f4dd0d96a11ef706c5257f5a902b
Vladimir Letunovskiu <Letunovskiu2011@yandex.ru>
2025-11-16 20:53:57 +0300

    Remove change log panel from admin dashboard
    Removed the change log panel from the dashboard.

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -37,9 +37,6 @@ ActiveAdmin.register_page 'Dashboard' do
         panel t('active_admin.dashboard_welcome.info') do
           para t 'active_admin.dashboard_welcome.call_to_action'
         end
-        panel t('active_admin.dashboard_welcome.change_log') do
-       
-        end
       end
     end
   end
