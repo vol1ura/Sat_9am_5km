7400cd1f79ee1659caa4d0deaa47dbaab365fd57
someoneunlicense <someoneunlicense@example.com>
2025-11-22 17:37:44 +0300

    Migrate to arctic_admin

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -2,13 +2,12 @@
 //= require activeadmin/quill_editor/quill
 //= require activeadmin/quill_editor_input
 //= require active_admin/searchable_select
-//= require admin/results_drag
 
-$(document).ready(function() {
+$(document).ready(function () {
   $('.datepicker').datepicker(
     {
-    firstDay: 1, // Monday
-    dateFormat: 'yy-mm-dd',
-    showAnim: 'slideDown'
-  });
+      firstDay: 1, // Monday
+      dateFormat: 'yy-mm-dd',
+      showAnim: 'slideDown'
+    });
 });
