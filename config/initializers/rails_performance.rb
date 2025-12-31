60d6ed99d97f076fb5a65719c3c96d2417277e93
Yuriy Volodin <vol1ura@gmail.com>
2025-12-31 23:44:37 +0300

    minor changes

--- config/initializers/rails_performance.rb
+++ config/initializers/rails_performance.rb
@@ -3,11 +3,11 @@ RailsPerformance.setup do |config|
 
   config.redis = Redis.new url: ENV.fetch('REDIS_URL', 'redis://127.0.0.1:6379/2')
 
-  config.http_basic_authentication_enabled = false
+  config.duration = 8.hours
 
   config.ignored_paths = %w[/rails/active_storage /storage /app_performance]
 
   config.custom_data_proc = proc do |env|
     { user_agent: Rack::Request.new(env).env['HTTP_USER_AGENT'] }
   end
-end if defined?(RailsPerformance)
+end
