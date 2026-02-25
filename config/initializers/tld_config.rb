8efd900e120e923b6d4f32696a280b10a6688de3
Vladimir Letunovskiy <kompotomanya@gmail.com>
2026-02-25 14:27:32 +0300

    prep ahead feat.s

--- config/initializers/tld_config.rb
+++ config/initializers/tld_config.rb
@@ -2,5 +2,5 @@
 
 # Configure allowed top-level domains for the application
 # This list is used in ApplicationController#top_level_domain
-ALLOWED_TLDS = %w[ru rs by].freeze
+ALLOWED_TLDS = %w[ru rs by kz].freeze
 DEFAULT_TLD = :ru
