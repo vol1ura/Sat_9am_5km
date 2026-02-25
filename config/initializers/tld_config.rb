3f8d2893fdb64174d5ea620fd9bd7dceefe72c73
Vladimir Letunovskiy <kompotomanya@gmail.com>
2026-02-25 14:31:17 +0300

    Ремайндер

--- config/initializers/tld_config.rb
+++ config/initializers/tld_config.rb
@@ -2,5 +2,6 @@
 
 # Configure allowed top-level domains for the application
 # This list is used in ApplicationController#top_level_domain
+# do not forget to add a new locale when creating a new domain.
 ALLOWED_TLDS = %w[ru rs by kz].freeze
 DEFAULT_TLD = :ru
