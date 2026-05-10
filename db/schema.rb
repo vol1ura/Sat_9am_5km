2f3f88934172d47c57eb79461df45c4cc25e1f53
SomeoneUnlicensed <kompotomanya@gmail.com>
2026-05-10 08:58:46 +0300

    migrationsupdate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_09_192000) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_09_230000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pg_stat_statements"
@@ -304,6 +304,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_09_192000) do
 
   create_table "wallet_pass_registrations", force: :cascade do |t|
     t.bigint "athlete_id", null: false
+    t.string "auth_token", null: false
     t.datetime "created_at", null: false
     t.string "device_library_identifier", null: false
     t.string "pass_type_identifier", null: false
