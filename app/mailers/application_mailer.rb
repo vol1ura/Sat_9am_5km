3843625207fe0b7d17a1fb28394fbe0f76691537
Yuriy Volodin <vol1ura@gmail.com>
2026-05-12 22:23:32 +0300

    update branding from Sat9am5km to S95

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: email_address_with_name(Rails.application.credentials.mailer.user_name, 'Sat9am5km')
+  default from: email_address_with_name(Rails.application.credentials.mailer.user_name, 'S95')
   layout 'mailer'
 end
